b685bbbe6f3cf7dafccbddad85d55324aa875c17
nobodyatroot <35878315+nobodyatroot@users.noreply.github.com>
2023-10-15 17:23:29 -0500

    Update MagazineDto.php

--- src/DTO/MagazineDto.php
+++ src/DTO/MagazineDto.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\DTO;
 
 use App\Entity\User;
+use App\Entity\Image;
 use App\Utils\RegPatterns;
 use App\Validator\Unique;
 use Doctrine\Common\Collections\Collection;
