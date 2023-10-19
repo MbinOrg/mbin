16522b210cc1be6fb1dfb5e09dd13f407c24473b
lilfade <lilfade.lor@gmail.com>
2023-10-18 21:13:13 -0500

    removal of files

--- src/Entity/Entry.php
+++ src/Entity/Entry.php
@@ -141,11 +141,11 @@ class Entry implements VotableInterface, CommentInterface, DomainInterface, Visi
         'persist',
     ], fetch: 'EXTRA_LAZY', orphanRemoval: true)]
     public Collection $badges;
-    #[OneToMany(mappedBy: 'entry', targetEntity: EntryCardanoTx::class, cascade: [
-        'remove',
-        'persist',
-    ], fetch: 'EXTRA_LAZY', orphanRemoval: true)]
-    public Collection $cardanoTx;
+    //#[OneToMany(mappedBy: 'entry', targetEntity: EntryBadge::class, cascade: [
+    //    'remove',
+    //    'persist',
+    //], fetch: 'EXTRA_LAZY', orphanRemoval: true)]
+    //public Collection $cardanoTx;
     public array $children = [];
     #[Id]
     #[GeneratedValue]
@@ -183,7 +183,7 @@ class Entry implements VotableInterface, CommentInterface, DomainInterface, Visi
         $this->notifications = new ArrayCollection();
         $this->viewCounters = new ArrayCollection();
         $this->badges = new ArrayCollection();
-        $this->cardanoTx = new ArrayCollection();
+        //$this->cardanoTx = new ArrayCollection();
 
         $user->addEntry($this);
 
