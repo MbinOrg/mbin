c071285edc40c859aa07e710a64ba1440b49d06d
Melroy van den Berg <melroy@melroy.org>
2023-10-22 19:43:42 +0200

    Fix symfony/stimulus-bundle recipe execution

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -6,13 +6,3 @@ export const app = startStimulusApp(require.context(
     true,
     /\.[jt]sx?$/
 ));
-import {startStimulusApp} from '@symfony/stimulus-bridge';
-
-// Registers Stimulus controllers from controllers.json and in the controllers/ directory
-const app = startStimulusApp(require.context(
-    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
-    true,
-    /\.(j|t)sx?$/
-));
-
-export {app};
