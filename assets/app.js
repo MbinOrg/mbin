159f426473b6c4377a08cab4bd5fa0b553dacdc7
Melroy van den Berg <melroy@melroy.org>
2023-10-24 17:54:41 +0200

    Improve error warning log pollution + composer recipes (Part 3) (#100)

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,8 @@
 import {Application} from '@hotwired/stimulus'
+import './bootstrap.js';
 import './styles/app.scss';
 import './utils/popover';
-// start the Stimulus application
-import './bootstrap';
 import '@github/markdown-toolbar-element'
 
+// start the Stimulus application
 Application.start()
