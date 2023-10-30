4ec2a86c7db7060810a6f1603a95cd2241234008
Melroy van den Berg <melroy@melroy.org>
2023-10-30 20:21:44 +0100

    Create object for metadata

--- src/Factory/ActivityPub/NodeInfoFactory.php
+++ src/Factory/ActivityPub/NodeInfoFactory.php
@@ -64,7 +64,7 @@ class NodeInfoFactory
                 'localComments' => $this->repository->countLocalComments(),
             ],
             'openRegistrations' => $this->settingsManager->get('KBIN_REGISTRATIONS_ENABLED'),
-            'metadata' => (object)[],
+            'metadata' => new stdClass, // new stdClass  will create a JSON object instead of an array
         ];
     }
 }
