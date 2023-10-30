5ee17574e4218d41ff60ba054e271596dd20dd69
Melroy van den Berg <melroy@melroy.org>
2023-10-30 15:34:29 +0000

    Introducing NodeInfo v2.1 support, plus refactoring

--- src/Controller/ActivityPub/NodeInfoController.php
+++ src/Controller/ActivityPub/NodeInfoController.php
@@ -10,26 +10,55 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class NodeInfoController
 {
+    private const NODE_REL_v20 = 'http://nodeinfo.diaspora.software/ns/schema/2.0';
+    private const NODE_REL_v21 = 'http://nodeinfo.diaspora.software/ns/schema/2.1';
+
     public function __construct(
         private readonly NodeInfoFactory $nodeInfoFactory,
         private readonly UrlGeneratorInterface $urlGenerator
     ) {
     }
 
+    /**
+     * Returning NodeInfo JSON response for path: .well-known/nodeinfo.
+     */
     public function nodeInfo(): JsonResponse
     {
-        return new JsonResponse([
-            'links' => [
-                [
-                    'rel' => NodeInfoFactory::NODE_REL,
-                    'href' => $this->urlGenerator->generate('ap_node_info_v2', [], UrlGeneratorInterface::ABSOLUTE_URL),
-                ],
-            ],
-        ]);
+        return new JsonResponse($this->getLinks());
     }
 
-    public function nodeInfoV2(): JsonResponse
+    /**
+     * Returning NodeInfo JSON response for path: nodeinfo/2.0.
+     */
+    public function nodeInfoV20(): JsonResponse
     {
-        return new JsonResponse($this->nodeInfoFactory->create());
+        return new JsonResponse($this->nodeInfoFactory->create('2.0'));
+    }
+
+    /**
+     * Returning NodeInfo JSON response for path: nodeinfo/2.1.
+     */
+    public function nodeInfoV21(): JsonResponse
+    {
+        return new JsonResponse($this->nodeInfoFactory->create('2.1'));
+    }
+
+    /**
+     * Get list of links for well-known nodeinfo.
+     */
+    private function getLinks(): array
+    {
+        return [
+            'links' => [
+                [
+                    'rel' => self::NODE_REL_V21,
+                    'href' => $this->urlGenerator->generate('ap_node_info_v21', [], UrlGeneratorInterface::ABSOLUTE_URL),
+                ],
+                [
+                    'rel' => self::NODE_REL_v20,
+                    'href' => $this->urlGenerator->generate('ap_node_info_v20', [], UrlGeneratorInterface::ABSOLUTE_URL),
+                ],
+            ],
+        ];
     }
 }
--- src/Factory/ActivityPub/NodeInfoFactory.php
+++ src/Factory/ActivityPub/NodeInfoFactory.php
@@ -9,9 +9,9 @@ use App\Service\SettingsManager;
 
 class NodeInfoFactory
 {
-    public const NODE_REL = 'http://nodeinfo.diaspora.software/ns/schema/2.0';
-    public const NODE_PROTOCOL = 'activitypub';
-    public const KBIN_HOMEPAGE = 'https://github.com/MbinOrg';
+    private const NODE_PROTOCOL = 'activitypub';
+    private const MBIN_REPOSITORY_URL = 'https://github.com/MbinOrg/mbin';
+    private const MBIN_VERSION = '1.0.0'; // TODO: Should be read from package.json or composer.json
 
     public function __construct(
         private readonly StatsContentRepository $repository,
@@ -19,14 +19,34 @@ class NodeInfoFactory
     ) {
     }
 
-    public function create(): array
+    /**
+     * Create and return a NodeInfo PHP array depending on the version input.
+     *
+     * @param string $version NodeInfo version string (eg. "2.0")
+     */
+    public function create(string $version): array
     {
+        switch ($version) {
+            case '2.0':
+                $software = [
+                    'name' => 'mbin',
+                    'version' => self::MBIN_VERSION,
+                ];
+                break;
+            case '2.1':
+            default:
+                // Used for 2.1 and as fallback
+                $software = [
+                    'name' => 'mbin',
+                    'version' => self::MBIN_VERSION,
+                    'repository' => self::MBIN_REPOSITORY_URL,
+                ];
+                break;
+        }
+
         return [
-            'version' => '2.0',
-            'software' => [
-                'name' => 'mbin',
-                'version' => '1.0.0',
-            ],
+            'version' => $version,
+            'software' => $software,
             'protocols' => [
                 self::NODE_PROTOCOL,
             ],
