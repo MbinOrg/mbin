773521aad065b68221d9e9d7e898e6ccbf6752c8
nobodyatroot <35878315+nobodyatroot@users.noreply.github.com>
2023-11-18 20:10:49 -0600

    Keep mappings

--- src/Entity/ApActivity.php
+++ src/Entity/ApActivity.php
@@ -10,6 +10,8 @@ use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
 
 #[Entity(repositoryClass: ApActivityRepository::class)]
 class ApActivity
