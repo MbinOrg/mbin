ef00fa61900d45b10a1a5cc532325c85ce2cc711
nobodyatroot <35878315+nobodyatroot@users.noreply.github.com>
2023-11-21 10:59:59 -0600

    improve readability of migration code

--- migrations/Version20231121010453.php
+++ migrations/Version20231121010453.php
@@ -12,40 +12,37 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20231121010453 extends AbstractMigration
 {
+    private const INDEXES = [
+        'entry_ap_id_lower_idx' => ['table' => 'entry', 'column' => 'lower(ap_id)'],
+        'entry_comment_ap_id_lower_idx' => ['table' => 'entry_comment', 'column' => 'lower(ap_id)'],
+        'magazine_ap_id_lower_idx' => ['table' => 'magazine', 'column' => 'lower(ap_id)'],
+        'magazine_ap_profile_id_lower_idx' => ['table' => 'magazine', 'column' => 'lower(ap_profile_id)'],
+        'magazine_name_lower_idx' => ['table' => 'magazine', 'column' => 'lower(name)'],
+        'magazine_title_lower_idx' => ['table' => 'magazine', 'column' => 'lower(title)'],
+        'post_ap_id_lower_idx' => ['table' => 'post', 'column' => 'lower(ap_id)'],
+        'post_comment_ap_id_lower_idx' => ['table' => 'post_comment', 'column' => 'lower(ap_id)'],
+        'user_ap_id_lower_idx' => ['table' => 'user', 'column' => 'lower(ap_id)'],
+        'user_ap_profile_id_lower_idx' => ['table' => 'user', 'column' => 'lower(ap_profile_id)'],
+        'user_email_lower_idx' => ['table' => 'user', 'column' => 'lower(email)'],
+        'user_username_lower_idx' => ['table' => 'user', 'column' => 'lower(username)'],
+    ];
+
     public function getDescription(): string
     {
-        return 'Introduce some db optimizations - sync with /kbin, Mbin specific changes';
+        return 'Introduce db optimizations - sync with /kbin, Mbin specific changes';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE INDEX user_username_lower_idx ON "user" (lower(username))');
-        $this->addSql('CREATE INDEX user_email_lower_idx ON "user" (lower(email))');
-        $this->addSql('CREATE INDEX magazine_ap_id_lower_idx ON magazine (lower(ap_id))');
-        $this->addSql('CREATE INDEX magazine_ap_profile_id_lower_idx ON magazine (lower(ap_profile_id))');
-        $this->addSql('CREATE INDEX user_ap_id_lower_idx ON "user" (lower(ap_id))');
-        $this->addSql('CREATE INDEX user_ap_profile_id_lower_idx ON "user" (lower(ap_profile_id))');
-        $this->addSql('CREATE INDEX magazine_name_lower_idx ON magazine (lower(name))');
-        $this->addSql('CREATE INDEX magazine_title_lower_idx ON magazine (lower(title))');
-        $this->addSql('CREATE INDEX entry_ap_id_lower_idx ON entry (lower(ap_id))');
-        $this->addSql('CREATE INDEX entry_comment_ap_id_lower_idx ON entry_comment (lower(ap_id))');
-        $this->addSql('CREATE INDEX post_ap_id_lower_idx ON post (lower(ap_id))');
-        $this->addSql('CREATE INDEX post_comment_ap_id_lower_idx ON post_comment (lower(ap_id))');
+        foreach (self::INDEXES as $index => $details) {
+            $this->addSql('CREATE INDEX '.$index.' ON "'.$details['table'].'" ('.$details['column'].')');
+        }
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP INDEX user_username_lower_idx');
-        $this->addSql('DROP INDEX user_email_lower_idx');
-        $this->addSql('DROP INDEX magazine_ap_id_lower_idx');
-        $this->addSql('DROP INDEX magazine_ap_profile_id_lower_idx');
-        $this->addSql('DROP INDEX user_ap_id_lower_idx');
-        $this->addSql('DROP INDEX user_ap_profile_id_lower_idx');
-        $this->addSql('DROP INDEX magazine_name_lower_idx');
-        $this->addSql('DROP INDEX magazine_title_lower_idx');
-        $this->addSql('DROP INDEX entry_ap_id_lower_idx');
-        $this->addSql('DROP INDEX entry_comment_ap_id_lower_idx');
-        $this->addSql('DROP INDEX post_ap_id_lower_idx');
-        $this->addSql('DROP INDEX post_comment_ap_id_lower_idx');
+        foreach (self::INDEXES as $index => $details) {
+            $this->addSql('DROP INDEX '.$index);
+        }
     }
 }
