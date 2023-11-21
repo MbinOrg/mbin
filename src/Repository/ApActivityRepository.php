4afc41c1d6aad613a60c252e5f172e5225c6a448
nobodyatroot <35878315+nobodyatroot@users.noreply.github.com>
2023-11-21 17:57:35 -0600

    remove order by id

--- src/Repository/ApActivityRepository.php
+++ src/Repository/ApActivityRepository.php
@@ -79,13 +79,13 @@ class ApActivityRepository extends ServiceEntityRepository
             END AS type
         FROM
             (
-                SELECT id, \'entry\' AS table_name FROM entry WHERE ap_id = :apId ORDER BY id LIMIT 1
+                SELECT id, \'entry\' AS table_name FROM entry WHERE ap_id = :apId LIMIT 1
                 UNION
-                SELECT id, \'entry_comment\' FROM entry_comment WHERE ap_id = :apId ORDER BY id LIMIT 1
+                SELECT id, \'entry_comment\' FROM entry_comment WHERE ap_id = :apId LIMIT 1
                 UNION
-                SELECT id, \'post\' FROM post WHERE ap_id = :apId ORDER BY id LIMIT 1
+                SELECT id, \'post\' FROM post WHERE ap_id = :apId LIMIT 1
                 UNION
-                SELECT id, \'post_comment\' FROM post_comment WHERE ap_id = :apId ORDER BY id LIMIT 1
+                SELECT id, \'post_comment\' FROM post_comment WHERE ap_id = :apId LIMIT 1
             ) AS combined_result)';
 
         $stmt = $conn->prepare($sql);
