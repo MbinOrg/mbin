af11dc490fc5e676249329966126eda860408e03
nobodyatroot <35878315+nobodyatroot@users.noreply.github.com>
2023-11-20 19:35:26 -0600

    update db optimizations - sync with /kbin, Mbin specific changes

--- migrations/Version20231121010453.php
+++ migrations/Version20231121010453.php
@@ -14,23 +14,23 @@ final class Version20231121010453 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Introduce some db optimizations - sync with /kbin';
+        return 'Introduce some db optimizations - sync with /kbin, Mbin specific changes';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE INDEX user_username_lower_idx ON "user" (lower(username))');
         $this->addSql('CREATE INDEX user_email_lower_idx ON "user" (lower(email))');
-        $this->addSql('CREATE INDEX magazine_ap_id_idx ON magazine (ap_id)');
-        $this->addSql('CREATE INDEX magazine_ap_profile_id_idx ON magazine (ap_profile_id)');
-        $this->addSql('CREATE INDEX user_ap_id_idx ON "user" (ap_id)');
-        $this->addSql('CREATE INDEX user_ap_profile_id_idx ON "user" (ap_profile_id)');
+        $this->addSql('CREATE INDEX magazine_ap_id_idx ON magazine (lower(ap_id))');
+        $this->addSql('CREATE INDEX magazine_ap_profile_id_idx ON magazine (lower(ap_profile_id))');
+        $this->addSql('CREATE INDEX user_ap_id_idx ON "user" (lower(ap_id))');
+        $this->addSql('CREATE INDEX user_ap_profile_id_idx ON "user" (lower(ap_profile_id))');
         $this->addSql('CREATE INDEX magazine_name_lower_idx ON magazine (lower(name))');
         $this->addSql('CREATE INDEX magazine_title_lower_idx ON magazine (lower(title))');
-        $this->addSql('CREATE INDEX entry_ap_id_idx ON entry (ap_id)');
-        $this->addSql('CREATE INDEX entry_comment_ap_id_idx ON entry_comment (ap_id)');
-        $this->addSql('CREATE INDEX post_ap_id_idx ON post (ap_id)');
-        $this->addSql('CREATE INDEX post_comment_ap_id_idx ON post_comment (ap_id)');
+        $this->addSql('CREATE INDEX entry_ap_id_idx ON entry (lower(ap_id))');
+        $this->addSql('CREATE INDEX entry_comment_ap_id_idx ON entry_comment (lower(ap_id))');
+        $this->addSql('CREATE INDEX post_ap_id_idx ON post (lower(ap_id))');
+        $this->addSql('CREATE INDEX post_comment_ap_id_idx ON post_comment (lower(ap_id))');
     }
 
     public function down(Schema $schema): void
