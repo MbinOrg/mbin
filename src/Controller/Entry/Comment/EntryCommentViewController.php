6c2f00afbf5e268155c921b0b1b14d3f152fb04a
e-five <146029455+e-five256@users.noreply.github.com>
2023-12-02 23:35:02 -0500

    add entry comment view controller and template

---
+++ src/Controller/Entry/Comment/EntryCommentViewController.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Entry\Comment;
+
+use App\Controller\AbstractController;
+use App\DTO\EntryCommentDto;
+use App\Entity\Entry;
+use App\Entity\EntryComment;
+use App\Entity\Magazine;
+use App\Form\EntryCommentType;
+use App\PageView\EntryCommentPageView;
+use App\Service\EntryCommentManager;
+use App\Service\IpResolver;
+use App\Service\MentionManager;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class EntryCommentViewController extends AbstractController
+{
+
+    public function __construct(
+        private readonly EntryCommentManager $manager,
+        private readonly RequestStack $requestStack,
+        private readonly IpResolver $ipResolver,
+        private readonly MentionManager $mentionManager
+    ) {
+    }
+
+    public function __invoke(
+        #[MapEntity(mapping: ['magazine_name' => 'name'])]
+        Magazine $magazine,
+        #[MapEntity(id: 'entry_id')]
+        Entry $entry,
+        #[MapEntity(id: 'parent_comment_id')]
+        ?EntryComment $parent,
+        Request $request,
+    ): Response {
+
+        // $criteria = new EntryCommentPageView($this->getPageNb($request));
+        // $criteria->entry = $entry;
+
+        return $this->render(
+            'entry/comment/view.html.twig',
+            [
+                'magazine' => $magazine,
+                'entry' => $entry,
+                'parent' => $parent,
+            ]
+        );
+    }
+}
