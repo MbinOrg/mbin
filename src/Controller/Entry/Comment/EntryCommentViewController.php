d64b9692dd03af1b3949ea1573174f81e379a703
e-five <146029455+e-five256@users.noreply.github.com>
2023-12-03 23:02:11 -0500

    remove unused imports

--- src/Controller/Entry/Comment/EntryCommentViewController.php
+++ src/Controller/Entry/Comment/EntryCommentViewController.php
@@ -5,31 +5,19 @@ declare(strict_types=1);
 namespace App\Controller\Entry\Comment;
 
 use App\Controller\AbstractController;
-use App\DTO\EntryCommentDto;
 use App\Entity\Entry;
 use App\Entity\EntryComment;
 use App\Entity\Magazine;
-use App\Form\EntryCommentType;
-use App\PageView\EntryCommentPageView;
-use App\Service\EntryCommentManager;
-use App\Service\IpResolver;
-use App\Service\MentionManager;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class EntryCommentViewController extends AbstractController
 {
 
     public function __construct(
-        private readonly EntryCommentManager $manager,
-        private readonly RequestStack $requestStack,
-        private readonly IpResolver $ipResolver,
-        private readonly MentionManager $mentionManager
+        private readonly RequestStack $requestStack
     ) {
     }
 
