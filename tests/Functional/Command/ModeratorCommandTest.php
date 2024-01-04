60cea14c58e2e86323c65667c0a91356efe7f8d6
nobodyatroot <35878315+nobodyatroot@users.noreply.github.com>
2024-01-04 12:12:30 -0600

    Add new files

---
+++ tests/Functional/Command/ModeratorCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Command;
+
+use App\DTO\UserDto;
+use App\Repository\UserRepository;
+use App\Service\UserManager;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class ModeratorCommandTest extends KernelTestCase
+{
+    private Command $command;
+    private ?UserRepository $repository;
+
+    public function testCreateUser(): void
+    {
+        $dto = (new UserDto())->create('actor', 'contact@example.com');
+        $dto->plainPassword = 'secret';
+
+        $this->getContainer()->get(UserManager::class)
+            ->create($dto, false);
+
+        $this->assertFalse($this->repository->findOneByUsername('actor')->isModerator());
+
+        $tester = new CommandTester($this->command);
+        $tester->execute(['username' => 'actor']);
+
+        $this->assertStringContainsString('Global moderator privileges have been granted.', $tester->getDisplay());
+        $this->assertTrue($this->repository->findOneByUsername('actor')->isModerator());
+    }
+
+    protected function setUp(): void
+    {
+        $application = new Application(self::bootKernel());
+
+        $this->command = $application->find('mbin:user:moderator');
+        $this->repository = $this->getContainer()->get(UserRepository::class);
+    }
+}
