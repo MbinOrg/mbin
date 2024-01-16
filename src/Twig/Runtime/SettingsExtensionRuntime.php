f88e41844e878218278f77409ecd4108bc9feafe
thepaperpilot <thepaperpilot@gmail.com>
2024-01-16 08:04:18 -0600

    More PR Feedback

--- src/Twig/Runtime/SettingsExtensionRuntime.php
+++ src/Twig/Runtime/SettingsExtensionRuntime.php
@@ -88,11 +88,6 @@ class SettingsExtensionRuntime implements RuntimeExtensionInterface
         return $this->settings->get('KBIN_FEDERATED_SEARCH_ONLY_LOGGEDIN');
     }
 
-    public function mbinSidebarSectionsLocalOnly(): bool
-    {
-        return $this->settings->get('MBIN_SIDEBAR_SECTIONS_LOCAL_ONLY');
-    }
-
     public function mbinCurrentVersion(): string
     {
         return $this->projectInfo->getVersion();
