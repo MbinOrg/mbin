0139e81927c34d5e57a24b15b35f354e3928217a
asdfzdfj <nonemchome@hotmail.com>
2024-01-18 21:53:34 +0700

    prune some likely unused ActivityPhp related code (#441)
    this patch removes most of the files/constructs that was copied from
    landrok/ActivityPhp, overall federation functionality shouldn't change.
    
    there's a decent chunk of code that was copied from the ActivityPhp
    library into kbin/mbin, but they're barely used in the codebase.
    
    the closest I've seen it in active use is in Webfinger factory and even
    then it was modified to use our own service and didn't actually call out
    to the library

--- src/Service/ActivityPub/ApHttpClient.php
+++ src/Service/ActivityPub/ApHttpClient.php
@@ -7,6 +7,7 @@ namespace App\Service\ActivityPub;
 use App\Entity\Magazine;
 use App\Entity\User;
 use App\Exception\InvalidApPostException;
+use App\Exception\InvalidWebfingerException;
 use App\Factory\ActivityPub\GroupFactory;
 use App\Factory\ActivityPub\PersonFactory;
 use App\Repository\MagazineRepository;
@@ -105,7 +106,7 @@ class ApHttpClient
                     if (null !== $r) {
                         $msg .= ', '.$r->getContent(false);
                     }
-                    throw new InvalidApPostException($msg);
+                    throw new InvalidWebfingerException($msg);
                 }
 
                 $item->expiresAt(new \DateTime('+1 hour'));
