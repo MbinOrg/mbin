f2680a20f8ba91e79aacccfb9b5f1b782b813171
asdfzdfj <nonemchome@hotmail.com>
2024-02-01 22:16:18 +0700

    rename user field to name

--- src/ActivityPub/ActorHandle.php
+++ src/ActivityPub/ActorHandle.php
@@ -6,11 +6,11 @@ namespace App\ActivityPub;
 
 class ActorHandle
 {
-    public const HANDLE_PATTERN = '/^(?P<prefix>[@!])?(?P<user>[\w\-\.]+)@(?P<host>[\w\.\-]+)(?P<port>:[\d]+)?$/';
+    public const HANDLE_PATTERN = '/^(?P<prefix>[@!])?(?P<name>[\w\-\.]+)@(?P<host>[\w\.\-]+)(?P<port>:[\d]+)?$/';
 
     public function __construct(
         public ?string $prefix = null,
-        public ?string $username = null,
+        public ?string $name = null,
         public ?string $host = null,
         public ?int $port = null,
     ) {
@@ -26,7 +26,7 @@ class ActorHandle
         if (preg_match(static::HANDLE_PATTERN, $handle, $match)) {
             $new = new static(
                 $match['prefix'] ?? null,
-                $match['user'],
+                $match['name'],
                 $match['host']
             );
             $new->setPort($match['port'] ?? null);
@@ -40,7 +40,7 @@ class ActorHandle
     public static function isHandle(string $handle)
     {
         if (preg_match(static::HANDLE_PATTERN, $handle, $matches)) {
-            return !empty($matches['user']) && !empty($matches['host']);
+            return !empty($matches['name']) && !empty($matches['host']);
         }
 
         return false;
@@ -57,9 +57,7 @@ class ActorHandle
         return !empty($this->port) ? ':'.$this->port : '';
     }
 
-    /**
-     * @param int|string|null $port port as either plain int or string formatted like ':9000'
-     */
+    /** @param int|string|null $port port as either plain int or string formatted like ':9000' */
     public function setPort(int|string|null $port)
     {
         if (\is_string($port)) {
@@ -71,14 +69,16 @@ class ActorHandle
         return $this;
     }
 
+    /** @return string the handle's domain and optionally port in the format `host[:port]` */
     public function getDomain(): string
     {
         return $this->host.$this->getPortString();
     }
 
+    /** @param ?string $domain the domain in the format `host[:port]` to set both handle's host and port */
     public function setDomain(?string $domain)
     {
-        $url = parse_url($domain);
+        $url = parse_url($domain ?? '');
 
         $this->host = $url['host'] ?? null;
         $this->port = $url['port'] ?? null;
@@ -88,22 +88,22 @@ class ActorHandle
 
     public function formatWithPrefix(?string $prefix): string
     {
-        return "{$prefix}{$this->username}@{$this->getDomain()}";
+        return "{$prefix}{$this->name}@{$this->getDomain()}";
     }
 
-    /** @return string handle in the form user@domain */
+    /** @return string handle in the form `name@domain` */
     public function plainHandle(): string
     {
         return $this->formatWithPrefix('');
     }
 
-    /** @return string handle in the form @user@domain */
+    /** @return string handle in the form `@name@domain` */
     public function atHandle(): string
     {
         return $this->formatWithPrefix('@');
     }
 
-    /** @return string handle in the form !user@domain */
+    /** @return string handle in the form `!name@domain` */
     public function bangHandle(): string
     {
         return $this->formatWithPrefix('!');
