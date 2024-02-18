7c53e67dbe50024a8749622e6289b91081514270
asdfzdfj <nonemchome@hotmail.com>
2024-02-18 20:59:31 +0700

    move service worker registration into main app bundle (#507)
    moved service worker registration into main app bundle, and also
    deferred the service worker registration a bit as recommended by
    https://web.dev/articles/service-workers-registration#improving_the_boilerplate,
    though our service worker doesn't seems to be doing anything much.
    
    this removes 1 inline script block from base template

--- assets/app.js
+++ assets/app.js
@@ -4,5 +4,11 @@ import './styles/app.scss';
 import './utils/popover';
 import '@github/markdown-toolbar-element'
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js');
+    });
+}
+
 // start the Stimulus application
 Application.start()
