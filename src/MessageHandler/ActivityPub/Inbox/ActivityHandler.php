MbinOrg/mbin

commit 9eddb84f8ed7f6737ed5f810fe1716e1f3c9b328

Announce posts and comments (#369)

src/MessageHandler/ActivityPub/Outbox/AnnounceHandler.php
@@ -4,18 +4,25 @@ declare(strict_types=1);
 
 namespace App\MessageHandler\ActivityPub\Outbox;
 
+use App\Entity\Entry;
+use App\Entity\EntryComment;
+use App\Entity\Magazine;
+use App\Entity\Post;
+use App\Entity\PostComment;
+use App\Entity\User;
 use App\Factory\ActivityPub\ActivityFactory;
 use App\Message\ActivityPub\Outbox\AnnounceMessage;
 use App\Message\ActivityPub\Outbox\DeliverMessage;
 use App\Repository\MagazineRepository;
 use App\Repository\UserRepository;
 use App\Service\ActivityPub\Wrapper\AnnounceWrapper;
+use App\Service\ActivityPub\Wrapper\CreateWrapper;
 use App\Service\ActivityPub\Wrapper\UndoWrapper;
 use App\Service\ActivityPubManager;
 use App\Service\SettingsManager;
 use Doctrine\ORM\EntityManagerInterface;
-use JetBrains\PhpStorm\ArrayShape;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Messenger\Exception\UnrecoverableMessageHandlingException;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsMessageHandler]
@@ -27,6 +34,7 @@ class AnnounceHandler
         private readonly EntityManagerInterface $entityManager,
         private readonly AnnounceWrapper $announceWrapper,
         private readonly UndoWrapper $undoWrapper,
+        private readonly CreateWrapper $createWrapper,
         private readonly ActivityPubManager $activityPubManager,
         private readonly ActivityFactory $activityFactory,
         private readonly MessageBusInterface $bus,
@@ -34,35 +42,46 @@ class AnnounceHandler
     ) {
     }
 
-    #[ArrayShape([
-        '@context' => 'string',
-        'id' => 'string',
-        'actor' => 'string',
-        'object' => 'string',
-    ])]
-    public function __invoke(
-        AnnounceMessage $message
-    ): void {
+    public function __invoke(AnnounceMessage $message): void
+    {
         if (!$this->settingsManager->get('KBIN_FEDERATION_ENABLED')) {
             return;
         }
 
-        $user = $this->userRepository->find($message->userId);
+        if (null !== $message->userId) {
+            $actor = $this->userRepository->find($message->userId);
+        } elseif (null !== $message->magazineId) {
+            $actor = $this->magazineRepository->find($message->magazineId);
+        } else {
+            throw new UnrecoverableMessageHandlingException('no actor was specified');
+        }
+
         $object = $this->entityManager->getRepository($message->objectType)->find($message->objectId);
 
         $activity = $this->announceWrapper->build(
-            $this->activityPubManager->getActorProfileId($user),
+            $this->activityPubManager->getActorProfileId($actor),
             $this->activityFactory->create($object),
         );
+
+        if ($object instanceof Entry or $object instanceof Post or $object instanceof EntryComment or $object instanceof PostComment) {
+            $wrapperObject = $this->createWrapper->build($object);
+            unset($wrapperObject['@context']);
+            $activity['object'] = $wrapperObject;
+        }
 
         if ($message->removeAnnounce) {
             $activity = $this->undoWrapper->build($activity);
         }
 
-        $this->deliver(array_filter($this->userRepository->findAudience($user)), $activity);
-        $this->deliver(array_filter($this->activityPubManager->createInboxesFromCC($activity, $user)), $activity);
-        $this->deliver(array_filter($this->magazineRepository->findAudience($object->magazine)), $activity);
-        $this->deliver([$object->user->apInboxUrl], $activity);
+        if ($actor instanceof User) {
+            $this->deliver(array_filter($this->userRepository->findAudience($actor)), $activity);
+            $this->deliver(array_filter($this->activityPubManager->createInboxesFromCC($activity, $actor)), $activity);
+            $this->deliver(array_filter($this->magazineRepository->findAudience($object->magazine)), $activity);
+            $this->deliver([$object->user->apInboxUrl], $activity);
+        } elseif ($actor instanceof Magazine) {
+            $createHost = parse_url($object->apId, PHP_URL_HOST);
+            $this->deliver(array_filter($this->magazineRepository->findAudience($actor), fn ($item) => null !== $item and $createHost !== parse_url($item, PHP_URL_HOST)), $activity);
+        }
     }
 
     private function deliver(array $followers, array $activity): void

src/Entity/User.php
@@ -11,6 +11,7 @@ use App\Entity\Traits\ActivityPubActorTrait;
 use App\Entity\Traits\CreatedAtTrait;
 use App\Entity\Traits\VisibilityTrait;
 use App\Repository\UserRepository;
+use App\Service\ActivityPub\ApHttpClient;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\Criteria;
@@ -29,6 +30,7 @@ use Scheb\TwoFactorBundle\Model\BackupCodeInterface;
 use Scheb\TwoFactorBundle\Model\Totp\TotpConfiguration;
 use Scheb\TwoFactorBundle\Model\Totp\TotpConfigurationInterface;
 use Scheb\TwoFactorBundle\Model\Totp\TwoFactorInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\User\EquatableInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -865,5 +867,23 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, Visibil
             ->where(Criteria::expr()->eq('magazine', $magazine));
 
         return $this->magazineOwnershipRequests->matching($criteria)->count() > 0;
+    }
+
+    public function getFollowerUrl(ApHttpClient $client, UrlGeneratorInterface $urlGenerator, bool $isRemote): ?string
+    {
+        if ($isRemote) {
+            $actorObject = $client->getActorObject($this->apProfileId);
+            if ($actorObject and isset($actorObject['followers']) and \is_string($actorObject['followers'])) {
+                return $actorObject['followers'];
+            }
+
+            return null;
+        } else {
+            return $urlGenerator->generate(
+                'ap_user_followers',
+                ['username' => $this->username],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            );
+        }
     }
 }

src/EventSubscriber/VoteHandleSubscriber.php
@@ -56,6 +56,7 @@ class VoteHandleSubscriber implements EventSubscriberInterface
             $this->bus->dispatch(
                 new AnnounceMessage(
                     $event->vote->user->getId(),
+                    null,
                     $event->votable->getId(),
                     \get_class($event->votable),
                 ),

src/Factory/ActivityPub/EntryCommentNoteFactory.php
@@ -45,6 +45,11 @@ class EntryCommentNoteFactory
             $tags[] = $comment->magazine->name;
         }
 
+        $cc = [$this->groupFactory->getActivityPubId($comment->magazine)];
+        if ($followersUrl = $comment->user->getFollowerUrl($this->client, $this->urlGenerator, null !== $comment->apId)) {
+            $cc[] = $followersUrl;
+        }
+
         $note = array_merge($note ?? [], [
             'type' => 'Note',
             'id' => $this->getActivityPubId($comment),
@@ -53,16 +58,7 @@ class EntryCommentNoteFactory
             'to' => [
                 ActivityPubActivityInterface::PUBLIC_URL,
             ],
-            'cc' => [
-                $this->groupFactory->getActivityPubId($comment->magazine),
-                $comment->apId
-                    ? ($this->client->getActorObject($comment->user->apProfileId)['followers']) ?? []
-                    : $this->urlGenerator->generate(
-                        'ap_user_followers',
-                        ['username' => $comment->user->username],
-                        UrlGeneratorInterface::ABSOLUTE_URL
-                    ),
-            ],
+            'cc' => $cc,
             'content' => $this->markdownConverter->convertToHtml($comment->body, [MarkdownConverter::RENDER_TARGET => RenderTarget::ActivityPub]),
             'mediaType' => 'text/html',
             'source' => $comment->body ? [
@@ -127,13 +123,7 @@ class EntryCommentNoteFactory
 
     private function getReplyTo(EntryComment $comment): string
     {
-        if ($comment->apId) {
-            return $comment->apId;
-        }
-
-        return $comment->parent ? $this->getActivityPubId($comment->parent) : $this->pageFactory->getActivityPubId(
-            $comment->entry
-        );
+        return $comment->parent ? $this->getActivityPubId($comment->parent) : $this->pageFactory->getActivityPubId($comment->entry);
     }
 
     private function getReplyToAuthor(EntryComment $comment): string

src/Factory/ActivityPub/EntryPageFactory.php
@@ -44,6 +44,11 @@ class EntryPageFactory
             $tags[] = $entry->magazine->name;
         }
 
+        $cc = [];
+        if ($followersUrl = $entry->user->getFollowerUrl($this->client, $this->urlGenerator, null !== $entry->apId)) {
+            $cc[] = $followersUrl;
+        }
+
         $page = array_merge($page ?? [], [
             'id' => $this->getActivityPubId($entry),
             'type' => 'Page',
@@ -53,15 +58,7 @@ class EntryPageFactory
                 $this->groupFactory->getActivityPubId($entry->magazine),
                 ActivityPubActivityInterface::PUBLIC_URL,
             ],
-            'cc' => [
-                $entry->apId
-                    ? ($this->client->getActorObject($entry->user->apProfileId)['followers']) ?? []
-                    : $this->urlGenerator->generate(
-                        'ap_user_followers',
-                        ['username' => $entry->user->username],
-                        UrlGeneratorInterface::ABSOLUTE_URL
-                    ),
-            ],
+            'cc' => $cc,
             'name' => $entry->title,
             'content' => $entry->body ? $this->markdownConverter->convertToHtml(
                 $entry->body,

src/Factory/ActivityPub/PostCommentNoteFactory.php
@@ -45,6 +45,11 @@ class PostCommentNoteFactory
             $tags[] = $comment->magazine->name;
         }
 
+        $cc = [$this->groupFactory->getActivityPubId($comment->magazine)];
+        if ($followersUrl = $comment->user->getFollowerUrl($this->client, $this->urlGenerator, null !== $comment->apId)) {
+            $cc[] = $followersUrl;
+        }
+
         $note = array_merge($note ?? [], [
             'id' => $this->getActivityPubId($comment),
             'type' => 'Note',
@@ -53,16 +58,7 @@ class PostCommentNoteFactory
             'to' => [
                 ActivityPubActivityInterface::PUBLIC_URL,
             ],
-            'cc' => [
-                $this->groupFactory->getActivityPubId($comment->magazine),
-                $comment->apId
-                    ? ($this->client->getActorObject($comment->user->apProfileId)['followers']) ?? []
-                    : $this->urlGenerator->generate(
-                        'ap_user_followers',
-                        ['username' => $comment->user->username],
-                        UrlGeneratorInterface::ABSOLUTE_URL
-                    ),
-            ],
+            'cc' => $cc,
             'sensitive' => $comment->post->isAdult(),
             'content' => $this->markdownConverter->convertToHtml(
                 $comment->body,
@@ -131,13 +127,7 @@ class PostCommentNoteFactory
 
     private function getReplyTo(PostComment $comment): string
     {
-        if ($comment->apId) {
-            return $comment->apId;
-        }
-
-        return $comment->parent ? $this->getActivityPubId($comment->parent) : $this->postNoteFactory->getActivityPubId(
-            $comment->post
-        );
+        return $comment->parent ? $this->getActivityPubId($comment->parent) : $this->postNoteFactory->getActivityPubId($comment->post);
     }
 
     private function getReplyToAuthor(PostComment $comment): string

src/Factory/ActivityPub/PostNoteFactory.php
@@ -51,6 +51,11 @@ class PostNoteFactory
             $tags
         );
 
+        $cc = [];
+        if ($followersUrl = $post->user->getFollowerUrl($this->client, $this->urlGenerator, null !== $post->apId)) {
+            $cc[] = $followersUrl;
+        }
+
         $note = array_merge($note ?? [], [
             'id' => $this->getActivityPubId($post),
             'type' => 'Note',
@@ -60,15 +65,7 @@ class PostNoteFactory
                 $this->groupFactory->getActivityPubId($post->magazine),
                 ActivityPubActivityInterface::PUBLIC_URL,
             ],
-            'cc' => [
-                $post->apId
-                    ? ($this->client->getActorObject($post->user->apProfileId)['followers']) ?? []
-                    : $this->urlGenerator->generate(
-                        'ap_user_followers',
-                        ['username' => $post->user->username],
-                        UrlGeneratorInterface::ABSOLUTE_URL
-                    ),
-            ],
+            'cc' => $cc,
             'sensitive' => $post->isAdult(),
             'stickied' => $post->sticky,
             'content' => $this->markdownConverter->convertToHtml(

src/Message/ActivityPub/Outbox/AnnounceMessage.php
@@ -9,7 +9,8 @@ use App\Message\Contracts\AsyncApMessageInterface;
 class AnnounceMessage implements AsyncApMessageInterface
 {
     public function __construct(
-        public int $userId,
+        public ?int $userId,
+        public ?int $magazineId,
         public int $objectId,
         public string $objectType,
         public bool $removeAnnounce = false

commit a4aa8e64e8b9d0118e2e52e017148a4df1460cbc

Fix boost regression fix the regression from 9eddb84f8ed7f6737ed5f810fe1716e1f3c9b328 and c13bf815c31ab415e8437e77213fefc353298f56 which broke boost federation

src/MessageHandler/ActivityPub/Inbox/ActivityHandler.php
@@ -99,13 +99,14 @@ readonly class ActivityHandler
                 $this->entityManager->persist($actor);
                 $this->entityManager->flush();
             }
+            // we check for an array here, because boosts are announces with an url (string) as the object
             if (\is_array($payload['object'])) {
                 $payload = $payload['object'];
                 $actor = $payload['actor'] ?? $payload['attributedTo'] ?? null;
                 if ($actor) {
                     $user = $this->manager->findActorOrCreate($actor);
                     if ($user instanceof User && null === $user->apId) {
-                        // don't do anything if we get an announce activity for something a local user did
+                        // don't do anything if we get an announce activity for something a local user did (unless it's a boost, see comment above)
                         $this->logger->warning('ignoring this message because it announces an activity from a local user');
 
                         return;

src/MessageHandler/ActivityPub/Outbox/AnnounceHandler.php
@@ -60,9 +60,10 @@ class AnnounceHandler
         $activity = $this->announceWrapper->build(
             $this->activityPubManager->getActorProfileId($actor),
             $this->activityFactory->create($object),
+            true
         );
 
-        if ($object instanceof Entry or $object instanceof Post or $object instanceof EntryComment or $object instanceof PostComment) {
+        if ($actor instanceof Magazine && ($object instanceof Entry || $object instanceof Post || $object instanceof EntryComment || $object instanceof PostComment)) {
             $wrapperObject = $this->createWrapper->build($object);
             unset($wrapperObject['@context']);
             $activity['object'] = $wrapperObject;

src/Service/ActivityPub/Wrapper/AnnounceWrapper.php
@@ -14,7 +14,14 @@ class AnnounceWrapper
     {
     }
 
-    public function build(string $user, array $object): array
+    /**
+     * @param string $user the actor doing the announce
+     * @param array $object the thing the actor is announcing
+     * @param bool $idAsObject use only the id of $object as the 'object' in the payload.
+     * This should only be true for user boosts
+     * @return array an announce activity
+     */
+    public function build(string $user, array $object, bool $idAsObject = false): array
     {
         $id = Uuid::v4()->toRfc4122();
 
@@ -29,7 +36,7 @@ class AnnounceWrapper
             'id' => $this->urlGenerator->generate('ap_object', ['id' => $id], UrlGeneratorInterface::ABSOLUTE_URL),
             'type' => 'Announce',
             'actor' => $user,
-            'object' => $object,
+            'object' => $idAsObject ? $object['id'] : $object,
             'to' => $to,
             'cc' => $object['cc'] ?? [],
             'published' => (new \DateTime())->format(DATE_ATOM),