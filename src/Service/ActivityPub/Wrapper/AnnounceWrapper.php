edea9a181dee2f494f32f088df9a907eaf237d91
BentiGorlich <benjamin_g@posteo.de>
2024-03-15 10:32:39 +0000

    Fix/boost regression (#579)
    Co-authored-by: debounced <35878315+nobodyatroot@users.noreply.github.com>

--- src/Service/ActivityPub/Wrapper/AnnounceWrapper.php
+++ src/Service/ActivityPub/Wrapper/AnnounceWrapper.php
@@ -14,7 +14,15 @@ class AnnounceWrapper
     {
     }
 
-    public function build(string $user, array $object): array
+    /**
+     * @param string $user       the actor doing the announce
+     * @param array  $object     the thing the actor is announcing
+     * @param bool   $idAsObject use only the id of $object as the 'object' in the payload.
+     *                           This should only be true for user boosts
+     *
+     * @return array an announce activity
+     */
+    public function build(string $user, array $object, bool $idAsObject = false): array
     {
         $id = Uuid::v4()->toRfc4122();
 
@@ -29,7 +37,7 @@ class AnnounceWrapper
             'id' => $this->urlGenerator->generate('ap_object', ['id' => $id], UrlGeneratorInterface::ABSOLUTE_URL),
             'type' => 'Announce',
             'actor' => $user,
-            'object' => $object,
+            'object' => $idAsObject ? $object['id'] : $object,
             'to' => $to,
             'cc' => $object['cc'] ?? [],
             'published' => (new \DateTime())->format(DATE_ATOM),
