e471a2761d67b169876280fdefa74503aee203e8
asdfzdfj <nonemchome@hotmail.com>
2024-03-18 20:05:36 +0700

    added lock when updating actor via UpdateActorHandler
    while working on incoming activity, the UpdateActorMessage could be
    fired more than once per actor, and even with #602 applied there could
    still be multiple UpdateActorHandler running at the same time updating
    the same actor, before the added guard could kick in
    
    this adds a bit of locking in UpdateActorHandler per actor url to try
    and ensure that only one handler is actually updating an actor at a
    time, and discards any following update actor message that comes after
    
    added the force parameter to UpdateActorMessage and handler to force
    actor update regardless of #602 last updated time (still have to get
    past the lock though)
    
    the `mbin:actor:update` command also got some jank in there fixed
    as they also used UpdateActorMessage to do its job, and the force
    parameter is also used here
    
    also try moving the fetched time guard in #602 up to the handler level
    the guard is alright but such guard should resides on the higher level
    before calling the update method

--- src/Service/ActivityPubManager.php
+++ src/Service/ActivityPubManager.php
@@ -166,10 +166,7 @@ class ActivityPubManager
                     $user = $this->createUser($actorUrl);
                 } else {
                     if (!$user->apFetchedAt || $user->apFetchedAt->modify('+1 hour') < (new \DateTime())) {
-                        try {
-                            $this->bus->dispatch(new UpdateActorMessage($user->apProfileId));
-                        } catch (\Exception $e) {
-                        }
+                        $this->bus->dispatch(new UpdateActorMessage($user->apProfileId));
                     }
                 }
 
@@ -185,10 +182,7 @@ class ActivityPubManager
                     $magazine = $this->createMagazine($actorUrl);
                 } else {
                     if (!$magazine->apFetchedAt || $magazine->apFetchedAt->modify('+1 hour') < (new \DateTime())) {
-                        try {
-                            $this->bus->dispatch(new UpdateActorMessage($magazine->apProfileId));
-                        } catch (\Exception $e) {
-                        }
+                        $this->bus->dispatch(new UpdateActorMessage($magazine->apProfileId));
                     }
                 }
 
@@ -290,10 +284,6 @@ class ActivityPubManager
     {
         $this->logger->info('updating user {name}', ['name' => $actorUrl]);
         $user = $this->userRepository->findOneBy(['apProfileId' => $actorUrl]);
-
-        if ($user instanceof User && $user->apFetchedAt > (new \DateTime())->modify('-1 hour')) {
-            return $user;
-        }
 
         $actor = $this->apHttpClient->getActorObject($actorUrl);
         if (!$actor || !\is_array($actor)) {
@@ -425,10 +415,6 @@ class ActivityPubManager
     {
         $this->logger->info('updating magazine "{magName}"', ['magName' => $actorUrl]);
         $magazine = $this->magazineRepository->findOneBy(['apProfileId' => $actorUrl]);
-
-        if ($magazine instanceof Magazine && $magazine->apFetchedAt > (new \DateTime())->modify('-1 hour')) {
-            return $magazine;
-        }
 
         $actor = $this->apHttpClient->getActorObject($actorUrl);
         // Check if actor isn't empty (not set/null/empty array/etc.)
