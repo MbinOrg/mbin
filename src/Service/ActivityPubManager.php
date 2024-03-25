a4a9aea58b8d864554ea6269cf37f77b289d461b
Melroy van den Berg <melroy@melroy.org>
2024-03-25 01:16:57 +0100

    Even better, check for instanceof User

--- src/Service/ActivityPubManager.php
+++ src/Service/ActivityPubManager.php
@@ -130,7 +130,7 @@ class ActivityPubManager
         if (false === filter_var($actorUrl, FILTER_VALIDATE_URL)) {
             if (!substr_count(ltrim($actorUrl, '@'), '@')) {
                 $user = $this->userRepository->findOneBy(['username' => ltrim($actorUrl, '@')]);
-                if (!empty($user)) {
+                if ($user instanceof User) {
                     if ($user->apFetchedAt->modify('+1 hour') < (new \DateTime())) {
                         $this->bus->dispatch(new UpdateActorMessage($user->apProfileId));
                     }
