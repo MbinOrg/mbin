1a4014070a1c4152681a4e920b0d05e5ad5cc019
e-five <146029455+e-five256@users.noreply.github.com>
2024-03-30 20:50:56 -0400

    use entry comment view url in entry comment notifications add event listener to mark notifications as read when viewed

--- src/Controller/Entry/Comment/EntryCommentViewController.php
+++ src/Controller/Entry/Comment/EntryCommentViewController.php
@@ -8,6 +8,8 @@ use App\Controller\AbstractController;
 use App\Entity\Entry;
 use App\Entity\EntryComment;
 use App\Entity\Magazine;
+use App\Event\Entry\EntryHasBeenSeenEvent;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -28,8 +30,14 @@ class EntryCommentViewController extends AbstractController
         Entry $entry,
         #[MapEntity(id: 'parent_comment_id')]
         ?EntryComment $parent,
+        EventDispatcherInterface $dispatcher,
         Request $request,
     ): Response {
+
+        // @TODO there is no entry comment has been seen event, maybe
+        // it should be added so one comment view does not mark all as read in the same entry
+        $dispatcher->dispatch(new EntryHasBeenSeenEvent($entry));
+
         return $this->render(
             'entry/comment/view.html.twig',
             [
