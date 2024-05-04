f517e4a4a81ddf42ccb525e9d5fbf64da030b083
e-five <146029455+e-five256@users.noreply.github.com>
2024-05-04 08:08:14 -0400

    Add admin panel option to enable/disable SSO only mode (#740)

--- src/Form/SettingsType.php
+++ src/Form/SettingsType.php
@@ -36,6 +36,7 @@ class SettingsType extends AbstractType
             ->add('KBIN_MERCURE_ENABLED', CheckboxType::class, ['required' => false])
             ->add('KBIN_FEDERATION_PAGE_ENABLED', CheckboxType::class, ['required' => false])
             ->add('KBIN_ADMIN_ONLY_OAUTH_CLIENTS', CheckboxType::class, ['required' => false])
+            ->add('MBIN_SSO_ONLY_MODE', CheckboxType::class, ['required' => false])
             ->add('MBIN_PRIVATE_INSTANCE', CheckboxType::class, ['required' => false])
             ->add('KBIN_FEDERATED_SEARCH_ONLY_LOGGEDIN', CheckboxType::class, ['required' => false])
             ->add('MBIN_SIDEBAR_SECTIONS_LOCAL_ONLY', CheckboxType::class, ['required' => false])
