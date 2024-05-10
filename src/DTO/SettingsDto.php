701d654d21d6100e882bfe1119a469ff304c068a
e-five <146029455+e-five256@users.noreply.github.com>
2024-05-10 19:52:58 -0400

    Restyle login/register social and action button sections (#706)

--- src/DTO/SettingsDto.php
+++ src/DTO/SettingsDto.php
@@ -34,7 +34,8 @@ class SettingsDto implements \JsonSerializable
         public bool $KBIN_FEDERATED_SEARCH_ONLY_LOGGEDIN,
         public bool $MBIN_SIDEBAR_SECTIONS_LOCAL_ONLY,
         public bool $MBIN_SSO_REGISTRATIONS_ENABLED,
-        public bool $MBIN_RESTRICT_MAGAZINE_CREATION
+        public bool $MBIN_RESTRICT_MAGAZINE_CREATION,
+        public bool $MBIN_SSO_SHOW_FIRST
     ) {
     }
 
@@ -64,6 +65,7 @@ class SettingsDto implements \JsonSerializable
         $dto->MBIN_SIDEBAR_SECTIONS_LOCAL_ONLY = $this->MBIN_SIDEBAR_SECTIONS_LOCAL_ONLY ?? $dto->MBIN_SIDEBAR_SECTIONS_LOCAL_ONLY;
         $dto->MBIN_SSO_REGISTRATIONS_ENABLED = $this->MBIN_SSO_REGISTRATIONS_ENABLED ?? $dto->MBIN_SSO_REGISTRATIONS_ENABLED;
         $dto->MBIN_RESTRICT_MAGAZINE_CREATION = $this->MBIN_RESTRICT_MAGAZINE_CREATION ?? $dto->MBIN_RESTRICT_MAGAZINE_CREATION;
+        $dto->MBIN_SSO_SHOW_FIRST = $this->MBIN_SSO_SHOW_FIRST ?? $dto->MBIN_SSO_SHOW_FIRST;
 
         return $dto;
     }
@@ -95,6 +97,7 @@ class SettingsDto implements \JsonSerializable
             'MBIN_SIDEBAR_SECTIONS_LOCAL_ONLY' => $this->MBIN_SIDEBAR_SECTIONS_LOCAL_ONLY,
             'MBIN_SSO_REGISTRATIONS_ENABLED' => $this->MBIN_SSO_REGISTRATIONS_ENABLED,
             'MBIN_RESTRICT_MAGAZINE_CREATION' => $this->MBIN_RESTRICT_MAGAZINE_CREATION,
+            'MBIN_SSO_SHOW_FIRST' => $this->MBIN_SSO_SHOW_FIRST,
         ];
     }
 }
