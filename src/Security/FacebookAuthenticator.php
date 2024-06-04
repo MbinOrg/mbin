52f4f6284fe138b68d0e03a59af54a29bca6960a
CocoPoops <cocopoops@cocopoops.com>
2024-06-04 00:44:47 +0000

    change sso authentication success route to front page

--- src/Security/FacebookAuthenticator.php
+++ src/Security/FacebookAuthenticator.php
@@ -152,7 +152,7 @@ class FacebookAuthenticator extends OAuth2Authenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        $targetUrl = $this->router->generate('user_settings_profile');
+        $targetUrl = $this->router->generate('front');
 
         return new RedirectResponse($targetUrl);
     }
