90aa7ab15269521b90f97d07de5c4aa79cba5d9b
nobodyatroot <35878315+nobodyatroot@users.noreply.github.com>
2024-06-28 21:45:52 -0500

    documentation wording

--- src/Exception/InvalidUserPublicKeyException.php
+++ src/Exception/InvalidUserPublicKeyException.php
@@ -7,7 +7,7 @@ namespace App\Exception;
 class InvalidUserPublicKeyException extends \Exception
 {
     /**
-     * @param string $apProfileId the url from which the activity was received
+     * @param string $apProfileId the url of the user where the key cannot be extracted, is malformed, or does not exist
      */
     public function __construct(public string $apProfileId, int $code = 0, \Throwable $previous = null)
     {
