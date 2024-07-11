4095fddc31d7a497997130c2ac23f97e7c7c30cb
Melroy van den Berg <melroy@melroy.org>
2024-07-11 21:32:53 +0200

    Add missing Symfony style import

--- src/Command/RemoveOldImagesCommand.php
+++ src/Command/RemoveOldImagesCommand.php
@@ -14,6 +14,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'mbin:images:delete',
