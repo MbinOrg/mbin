55b54c19b07898ccba286376ef83f14843417fcc
nobodyatroot <35878315+nobodyatroot@users.noreply.github.com>
2024-08-06 08:48:17 -0500

    add user password set/reset command

---
+++ src/Command/UserPasswordCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Repository\UserRepository;
+use App\Service\UserManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'mbin:user:password',
+    description: 'This command allows you to manually set or reset a users password.',
+)]
+class UserPasswordCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserPasswordHasherInterface $userPasswordHasher,
+        private readonly UserRepository $repository,
+        private readonly UserManager $manager
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('username', InputArgument::REQUIRED)
+            ->addArgument('password', InputArgument::REQUIRED);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $password = $input->getArgument('password');
+        $user = $this->repository->findOneByUsername($input->getArgument('username'));
+
+        if (!$user) {
+            $io->error('User does not exist!');
+
+            return Command::FAILURE;
+        }
+
+        // Encode(hash) the plain password, and set it.
+        $encodedPassword = $this->userPasswordHasher->hashPassword(
+            $user,
+            $password
+        );
+
+        $user->setPassword($encodedPassword);
+        $this->entityManager->flush();
+
+        return Command::SUCCESS;
+    }
+}
