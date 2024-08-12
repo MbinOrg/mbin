e5148e596f6f07549490f758ae1607c8faeabc19
debounced <35878315+nobodyatroot@users.noreply.github.com>
2024-08-12 18:00:49 -0500

    Update src/Entity/Magazine.php
    Co-authored-by: BentiGorlich <benjamin_g@posteo.de>

--- src/Entity/Magazine.php
+++ src/Entity/Magazine.php
@@ -367,7 +367,7 @@ class Magazine implements VisibilityInterface, ActivityPubActorInterface, ApiRes
 
     public function softDelete(): void
     {
-        $this->markedForDeletionAt = date_add(new \DateTime(), new \DateInterval('P30D'));
+        $this->markedForDeletionAt = new \DateTime('now + 30days');
         $this->visibility = VisibilityInterface::VISIBILITY_SOFT_DELETED;
     }
 
