022c25923556b0984b954ae60ceb01751a48524c
Melroy van den Berg <melroy@melroy.org>
2024-09-19 11:49:40 +0200

    Clean-up

--- src/Command/RemoveFailedMessages.php
+++ src/Command/RemoveFailedMessages.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Entity\Post;
-use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -40,5 +38,5 @@ class RemoveFailedMessanges extends Command
             'DELETE FROM messenger_messages WHERE queue_name = ?',
             ['failed']
         );
-    }    
+    }
 }
