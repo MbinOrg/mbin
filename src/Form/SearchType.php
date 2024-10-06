cc590cc6d306e9375409f42fbb3df6681d794c8b
BentiGorlich <benjamin_g@posteo.de>
2024-10-06 17:09:57 +0200

    Improve search - only search a specific magazine - only search posts from a specific user - only search a specific type: threads, microblogs or both

---
+++ src/Form/SearchType.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Form\Type\MagazineAutocompleteType;
+use App\Form\Type\UserAutocompleteType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->setMethod('GET')
+            ->add('q', TextType::class, ['required' => true])
+            ->add('magazine', MagazineAutocompleteType::class, ['required' => false])
+            ->add('user', UserAutocompleteType::class, ['required' => false])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'search_type_both' => null,
+                    'search_type_entry' => 'entry',
+                    'search_type_post' => 'post',
+                ],
+            ]);
+    }
+}
