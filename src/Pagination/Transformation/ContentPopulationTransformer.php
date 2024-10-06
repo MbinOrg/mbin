d4a5baeb5b5d84b533289e6ef156237eccaba505
BentiGorlich <benjamin_g@posteo.de>
2024-10-06 14:16:11 +0200

    Introducing bookmarks (#1095)
    Co-authored-by: Melroy van den Berg <melroy@melroy.org>

--- src/Pagination/Transformation/ContentPopulationTransformer.php
+++ src/Pagination/Transformation/ContentPopulationTransformer.php
@@ -19,6 +19,7 @@ class ContentPopulationTransformer implements ResultTransformer
 
     public function transform(iterable $input): iterable
     {
+        $positionsArray = $this->buildPositionArray($input);
         $entries = $this->entityManager->getRepository(Entry::class)->findBy(
             ['id' => $this->getOverviewIds((array) $input, 'entry')]
         );
@@ -32,10 +33,7 @@ class ContentPopulationTransformer implements ResultTransformer
             ['id' => $this->getOverviewIds((array) $input, 'post_comment')]
         );
 
-        $result = array_merge($entries, $entryComments, $post, $postComment);
-        uasort($result, fn ($a, $b) => $a->getCreatedAt() > $b->getCreatedAt() ? -1 : 1);
-
-        return $result;
+        return $this->applyPositions($positionsArray, $entries, $entryComments, $post, $postComment);
     }
 
     private function getOverviewIds(array $result, string $type): array
@@ -43,5 +41,68 @@ class ContentPopulationTransformer implements ResultTransformer
         $result = array_filter($result, fn ($subject) => $subject['type'] === $type);
 
         return array_map(fn ($subject) => $subject['id'], $result);
+    }
+
+    /**
+     * @return int[][]
+     */
+    private function buildPositionArray(iterable $input): array
+    {
+        $entryPositions = [];
+        $entryCommentPositions = [];
+        $postPositions = [];
+        $postCommentPositions = [];
+        $i = 0;
+        foreach ($input as $current) {
+            switch ($current['type']) {
+                case 'entry':
+                    $entryPositions[$current['id']] = $i;
+                    break;
+                case 'entry_comment':
+                    $entryCommentPositions[$current['id']] = $i;
+                    break;
+                case 'post':
+                    $postPositions[$current['id']] = $i;
+                    break;
+                case 'post_comment':
+                    $postCommentPositions[$current['id']] = $i;
+                    break;
+            }
+            ++$i;
+        }
+
+        return [
+            'entry' => $entryPositions,
+            'entry_comment' => $entryCommentPositions,
+            'post' => $postPositions,
+            'post_comment' => $postCommentPositions,
+        ];
+    }
+
+    /**
+     * @param int[][]        $positionsArray
+     * @param Entry[]        $entries
+     * @param EntryComment[] $entryComments
+     * @param Post[]         $posts
+     * @param PostComment[]  $postComments
+     */
+    private function applyPositions(array $positionsArray, array $entries, array $entryComments, array $posts, array $postComments): array
+    {
+        $result = [];
+        foreach ($entries as $entry) {
+            $result[$positionsArray['entry'][$entry->getId()]] = $entry;
+        }
+        foreach ($entryComments as $entryComment) {
+            $result[$positionsArray['entry_comment'][$entryComment->getId()]] = $entryComment;
+        }
+        foreach ($posts as $post) {
+            $result[$positionsArray['post'][$post->getId()]] = $post;
+        }
+        foreach ($postComments as $postComment) {
+            $result[$positionsArray['post_comment'][$postComment->getId()]] = $postComment;
+        }
+        ksort($result, SORT_NUMERIC);
+
+        return $result;
     }
 }
