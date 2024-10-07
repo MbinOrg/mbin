1c6dbd7f3c0705007fc24c68eb9a24555134f060
BentiGorlich <benjamin_g@posteo.de>
2024-10-07 20:55:25 +0200

    Introduce the activity table Whenever an actor makes an activity (like, announce, etc.) we now save it in our db, so the url we pass to other activity pub servers is valid and can return the correct json. All the factory and wrapper classes now return an `Activity` entity that can be converted to json using the new `ActivityJsonBuilder`

--- src/MessageHandler/ActivityPub/Outbox/GenericAnnounceHandler.php
+++ src/MessageHandler/ActivityPub/Outbox/GenericAnnounceHandler.php
@@ -7,13 +7,16 @@ namespace App\MessageHandler\ActivityPub\Outbox;
 use App\Message\ActivityPub\Outbox\GenericAnnounceMessage;
 use App\Message\Contracts\MessageInterface;
 use App\MessageHandler\MbinMessageHandler;
+use App\Repository\ActivityRepository;
 use App\Repository\MagazineRepository;
+use App\Service\ActivityPub\ActivityJsonBuilder;
 use App\Service\ActivityPub\Wrapper\AnnounceWrapper;
 use App\Service\DeliverManager;
 use App\Service\SettingsManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Uid\Uuid;
 
 #[AsMessageHandler]
 class GenericAnnounceHandler extends MbinMessageHandler
@@ -25,6 +28,8 @@ class GenericAnnounceHandler extends MbinMessageHandler
         private readonly MagazineRepository $magazineRepository,
         private readonly AnnounceWrapper $announceWrapper,
         private readonly DeliverManager $deliverManager,
+        private readonly ActivityRepository $activityRepository,
+        private readonly ActivityJsonBuilder $activityJsonBuilder,
     ) {
         parent::__construct($this->entityManager);
     }
@@ -42,13 +47,23 @@ class GenericAnnounceHandler extends MbinMessageHandler
         if (!($message instanceof GenericAnnounceMessage)) {
             throw new \LogicException();
         }
+
         $magazine = $this->magazineRepository->find($message->announcingMagazineId);
         if (null !== $magazine->apId) {
             return;
         }
-        $magazineUrl = $this->urlGenerator->generate('ap_magazine', ['name' => $magazine->name], UrlGeneratorInterface::ABSOLUTE_URL);
-        $announce = $this->announceWrapper->build($magazineUrl, $message->payloadToAnnounce);
+
+        if (null !== $message->innerActivityUUID) {
+            $object = $this->activityRepository->findOneBy(['uuid' => Uuid::fromString($message->innerActivityUUID)]);
+        } elseif (null !== $message->innerActivityUrl) {
+            $object = $message->innerActivityUrl;
+        } else {
+            throw new \LogicException('expect at least one of innerActivityUUID or innerActivityUrl to not be null');
+        }
+
+        $announce = $this->announceWrapper->build($magazine, $object);
+        $json = $this->activityJsonBuilder->buildActivityJson($announce);
         $inboxes = array_filter($this->magazineRepository->findAudience($magazine), fn ($item) => null !== $item && $item !== $message->sourceInstance);
-        $this->deliverManager->deliver($inboxes, $announce);
+        $this->deliverManager->deliver($inboxes, $json);
     }
 }
