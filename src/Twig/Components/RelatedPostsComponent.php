bc172698a411b53b6da5c069d5a7ef4c69bdabc1
BentiGorlich <benjamin_g@posteo.de>
2024-10-13 17:36:04 +0200

    Make related panels respect blocks (#1183)

--- src/Twig/Components/RelatedPostsComponent.php
+++ src/Twig/Components/RelatedPostsComponent.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Twig\Components;
 
 use App\Entity\Post;
+use App\Entity\User;
 use App\Repository\PostRepository;
 use App\Service\MentionManager;
 use App\Service\SettingsManager;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
@@ -31,6 +33,7 @@ final class RelatedPostsComponent
         private readonly CacheInterface $cache,
         private readonly SettingsManager $settingsManager,
         private readonly MentionManager $mentionManager,
+        private readonly Security $security,
     ) {
     }
 
@@ -46,21 +49,25 @@ final class RelatedPostsComponent
             $this->type = self::TYPE_MAGAZINE;
         }
 
+        /** @var User|null $user */
+        $user = $this->security->getUser();
+
         $postId = $this->post?->getId();
         $magazine = str_replace('@', '', $magazine ?? '');
 
         $postIds = $this->cache->get(
-            "related_posts_{$magazine}_{$tag}_{$postId}_{$this->type}_{$this->settingsManager->getLocale()}",
-            function (ItemInterface $item) use ($magazine, $tag) {
+            "related_posts_{$magazine}_{$tag}_{$postId}_{$this->type}_{$this->settingsManager->getLocale()}_{$user?->getId()}",
+            function (ItemInterface $item) use ($magazine, $tag, $user) {
                 $item->expiresAfter(60 * 5); // 5 minutes
 
                 $posts = match ($this->type) {
-                    self::TYPE_TAG => $this->repository->findRelatedByMagazine($tag, $this->limit + 20),
+                    self::TYPE_TAG => $this->repository->findRelatedByMagazine($tag, $this->limit + 20, user: $user),
                     self::TYPE_MAGAZINE => $this->repository->findRelatedByTag(
                         $this->mentionManager->getUsername($magazine),
-                        $this->limit + 20
+                        $this->limit + 20,
+                        user: $user
                     ),
-                    default => $this->repository->findLast($this->limit + 150),
+                    default => $this->repository->findLast($this->limit + 150, user: $user),
                 };
 
                 $posts = array_filter($posts, fn (Post $p) => !$p->isAdult && !$p->magazine->isAdult);
