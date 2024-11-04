b17470c46747a2a360e9a0da15bb24576ebbe8b8
Melroy van den Berg <melroy@melroy.org>
2024-11-04 18:57:55 +0100

    Fix dead instance query by allowing lastSuccessfulDeliver is null

--- src/Repository/InstanceRepository.php
+++ src/Repository/InstanceRepository.php
@@ -84,7 +84,7 @@ class InstanceRepository extends ServiceEntityRepository
     {
         return $this->createQueryBuilder('i')
             ->where('i.failedDelivers >= :numToDead')
-            ->andWhere('i.lastSuccessfulDeliver < :dateBeforeDead')
+            ->andWhere('i.lastSuccessfulDeliver < :dateBeforeDead OR i.lastSuccessfulDeliver IS NULL')
             ->setParameter('numToDead', Instance::NUMBER_OF_FAILED_DELIVERS_UNTIL_DEAD)
             ->setParameter('dateBeforeDead', Instance::getDateBeforeDead())
             ->orderBy('i.id')
