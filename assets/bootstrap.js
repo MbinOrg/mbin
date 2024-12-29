f2f6e73f9d4546b2fa88c2427b2ae3a98afc8785
Garrett W. <gw@garrettw.net>
2024-12-29 16:00:20 -0600

    Remove require from bootstrap.js
    Parsing as ES module fails without this change

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,7 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
+export const app = startStimulusApp(import.meta.webpackContext(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
     /\.[jt]sx?$/,
