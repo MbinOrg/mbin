943cdd51c09e8577f3569bc0177454d77730e48b
Garrett W. <gw@garrettw.net>
2024-12-30 03:13:05 +0000

    taking a different strategy
    "type: module" in package.json seems to break too much stuff so instead, explicitly specify that the eslint config is a module and leave everything else alone.

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,7 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(import.meta.webpackContext(
+export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
     /\.[jt]sx?$/,
