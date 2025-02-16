c76e7fc7bd953027cdb4cfa0a7e9d1a9fac45cfe
BentiGorlich <benjamin_g@posteo.de>
2025-02-16 00:03:28 +0000

    Always put thumb images in a lightbox (#1406)
    Co-authored-by: Melroy van den Berg <melroy@melroy.org>

--- assets/controllers/lightbox_controller.js
+++ assets/controllers/lightbox_controller.js
@@ -4,7 +4,12 @@ import GLightbox from 'glightbox';
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
     connect() {
-        const params = { selector: '.thumb', openEffect: 'none', closeEffect: 'none', slideEffect: 'none' };
+        const params = {
+            selector: '.thumb',
+            openEffect: 'none',
+            closeEffect: 'none',
+            touchNavigation: true,
+        };
         GLightbox(params);
     }
 }
