056886e8d9871e050df55fd44aa999b0c9995ad2
jwr1 <47087725+jwr1@users.noreply.github.com>
2025-04-03 23:17:56 -0400

    Small fixes

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,10 +2,6 @@
 set -e
 
 if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
-		composer install --prefer-dist --no-progress --no-interaction
-	fi
-
 	# Display information about the current project
 	# Or about an error in project initialization
 	php bin/console -V
