473688dffbf11fbbb4b0835ce052671326479755
jwr1 <47087725+jwr1@users.noreply.github.com>
2025-04-08 21:54:58 -0400

    Fix dev docker dev server

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,25 +1,29 @@
 #!/bin/sh
 set -e
 
-
-# Additional Mbin docker configurations
-
-# Use 301 response for image redirects to reduce server load
-sed -i 's|redirect_response_code: 302|redirect_response_code: 301|' config/packages/liip_imagine.yaml
-
-# Use S3 file system adapter when S3_KEY env var is not empty
-if [ -n "$S3_KEY" ]; then
-	sed -i 's|adapter: default_adapter|adapter: kbin.s3_adapter|' config/packages/oneup_flysystem.yaml
-fi
-
-# Needed to apply the above config changes
-php bin/console cache:clear
-
-
 if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
+	# Install dependencies if missing (needed for development)
+	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
+		composer install --prefer-dist --no-progress --no-interaction
+	fi
+
 	# Display information about the current project
 	# Or about an error in project initialization
 	php bin/console -V
+
+	# Additional Mbin docker configurations (only for production)
+	if [ "$APP_ENV" = 'prod' ]; then
+		# Use 301 response for image redirects to reduce server load
+		sed -i 's|redirect_response_code: 302|redirect_response_code: 301|' config/packages/liip_imagine.yaml
+
+		# Use S3 file system adapter when S3_KEY env var is not empty
+		if [ -n "$S3_KEY" ]; then
+			sed -i 's|adapter: default_adapter|adapter: kbin.s3_adapter|' config/packages/oneup_flysystem.yaml
+		fi
+	fi
+
+	# Needed to apply the above config changes
+	php bin/console cache:clear
 
 	if grep -q ^DATABASE_URL= .env; then
 		echo 'Waiting for database to be ready...'
