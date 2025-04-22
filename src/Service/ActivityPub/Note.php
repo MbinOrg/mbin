de7ef95716857ff27f50d9ce98dcef143832a3cc
BentiGorlich <benjamin_g@posteo.de>
2025-04-23 00:09:31 +0200

    Add a more useful message

--- src/Service/ActivityPub/Note.php
+++ src/Service/ActivityPub/Note.php
@@ -12,6 +12,7 @@ use App\Entity\Contracts\ActivityPubActivityInterface;
 use App\Entity\Contracts\VisibilityInterface;
 use App\Entity\Entry;
 use App\Entity\EntryComment;
+use App\Entity\Magazine;
 use App\Entity\Post;
 use App\Entity\PostComment;
 use App\Entity\User;
@@ -163,8 +164,10 @@ class Note
             $dto->apShareCount = $this->activityPubManager->extractRemoteShareCount($object);
 
             return $this->entryCommentManager->create($dto, $actor, false);
+        } elseif ($actor instanceof Magazine) {
+            throw new UnrecoverableMessageHandlingException('Actor "'.$object['attributedTo'].'" is not a user, but a magazine for post "'.$dto->apId.'".');
         } else {
-            throw new UnrecoverableMessageHandlingException('Actor could not be found for entry comment.');
+            throw new UnrecoverableMessageHandlingException('Actor "'.$object['attributedTo'].'"could not be found for post "'.$dto->apId.'".');
         }
     }
 
@@ -250,8 +253,10 @@ class Note
             $dto->apShareCount = $this->activityPubManager->extractRemoteShareCount($object);
 
             return $this->postManager->create($dto, $actor, false, $stickyIt);
+        } elseif ($actor instanceof Magazine) {
+            throw new UnrecoverableMessageHandlingException('Actor "'.$object['attributedTo'].'" is not a user, but a magazine for post "'.$dto->apId.'".');
         } else {
-            throw new UnrecoverableMessageHandlingException('Actor could not be found for post.');
+            throw new UnrecoverableMessageHandlingException('Actor "'.$object['attributedTo'].'"could not be found for post "'.$dto->apId.'".');
         }
     }
 
@@ -308,8 +313,10 @@ class Note
             $dto->apShareCount = $this->activityPubManager->extractRemoteShareCount($object);
 
             return $this->postCommentManager->create($dto, $actor, false);
+        } elseif ($actor instanceof Magazine) {
+            throw new UnrecoverableMessageHandlingException('Actor "'.$object['attributedTo'].'" is not a user, but a magazine for post "'.$dto->apId.'".');
         } else {
-            throw new UnrecoverableMessageHandlingException('Actor could not be found for post comment.');
+            throw new UnrecoverableMessageHandlingException('Actor "'.$object['attributedTo'].'"could not be found for post "'.$dto->apId.'".');
         }
     }
 }
