2d0845bd8f5caba90f22674681609cdcb53fe3fc
BentiGorlich <benjamin_g@posteo.de>
2025-04-30 13:34:47 +0200

    Fix linter and unit tests

--- src/DTO/EntryCommentRequestDto.php
+++ src/DTO/EntryCommentRequestDto.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\DTO;
 
-use App\Service\MentionManager;
 use App\Service\SettingsManager;
 use OpenApi\Attributes as OA;
 
--- tests/Functional/ActivityPub/Inbox/AcceptHandlerTest.php
+++ tests/Functional/ActivityPub/Inbox/AcceptHandlerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Functional\ActivityPub\Inbox;
 
 use App\Tests\Functional\ActivityPub\ActivityPubFunctionalTestCase;
