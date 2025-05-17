0c1ef142e42545b88e0296e06d4fc4877b4f05c9
Melroy van den Berg <melroy@melroy.org>
2025-05-17 13:06:09 +0200

    Add emoji picker (#1588)
    Co-authored-by: BentiGorlich <benjamin_g@posteo.de>

--- assets/controllers/markdown_toolbar_controller.js
+++ assets/controllers/markdown_toolbar_controller.js
@@ -3,6 +3,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import { Controller } from '@hotwired/stimulus';
+import { computePosition, flip, shift, limitShift, autoUpdate } from '@floating-ui/dom';
+import 'emoji-picker-element';
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
@@ -35,5 +37,55 @@ ${spoilerBody}
         const spoilerTitlePosition = contentBeforeCursor.length + '::: spoiler '.length + 1;
         input.setSelectionRange(spoilerTitlePosition, spoilerTitlePosition);
         input.focus();
+    }
+
+    toggleEmojiPicker(event) {
+        event.preventDefault();
+
+        const button = event.currentTarget;
+        const input = document.getElementById(this.element.getAttribute('for'));
+        const tooltip = document.querySelector('#tooltip');
+        const emojiPicker = document.querySelector('#emoji-picker');
+
+        // Remove any existing event listener
+        if (this.emojiClickHandler) {
+            emojiPicker.removeEventListener('emoji-click', this.emojiClickHandler);
+        }
+
+        if(!this.cleanupTooltip) {
+            this.cleanupTooltip = autoUpdate(button, tooltip, () => {
+                computePosition(button, tooltip, {
+                    placement: 'bottom',
+                    middleware: [flip(), shift({limiter: limitShift()})],
+                }).then(({x, y}) => {
+                    Object.assign(tooltip.style, {
+                        left: `${x}px`,
+                        top: `${y}px`,
+                    });
+                });
+            });
+        }
+
+        tooltip.classList.toggle('shown');
+
+        if (tooltip.classList.contains('shown')) {
+            this.emojiClickHandler = (event) => {
+                const emoji = event.detail.emoji.unicode;
+                const start = input.selectionStart;
+                const end = input.selectionEnd;
+
+                input.value = input.value.slice(0, start) + emoji + input.value.slice(end);
+                const emojiPosition = start + emoji.length;
+                input.setSelectionRange(emojiPosition, emojiPosition);
+                input.focus();
+
+                tooltip.classList.remove('shown');
+                this.cleanupTooltip();
+                emojiPicker.removeEventListener('emoji-click', this.emojiClickHandler);
+                this.emojiClickHandler = null;
+            };
+
+            emojiPicker.addEventListener('emoji-click', this.emojiClickHandler);
+        }
     }
 }
