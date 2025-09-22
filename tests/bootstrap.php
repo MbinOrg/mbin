88b0f88e08b2cf1b879ea5af8a6cf5fe8c83a68f
Melroy van den Berg <melroy@melroy.org>
2025-09-22 16:55:11 +0200

    Ran phpunit/phpunit recipe

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,72 +1,13 @@
 <?php
 
-declare(strict_types=1);
-
-use App\Kernel;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Dotenv\Dotenv;
 
-require \dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(\dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
-}
-
-function bootstrapDatabase(): void
-{
-    $kernel = new Kernel('test', true);
-    $kernel->boot();
-
-    $application = new Application($kernel);
-    $application->setAutoExit(false);
-
-    $application->run(new ArrayInput([
-        'command' => 'cache:pool:clear',
-        '--all' => '1',
-        '--no-interaction' => true,
-    ]));
-
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:database:drop',
-        '--if-exists' => '1',
-        '--force' => '1',
-    ]));
-
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:database:create',
-    ]));
-
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:migrations:migrate',
-        '--no-interaction' => true,
-    ]));
-
-    $application->run(new ArrayInput([
-        'command' => 'mbin:ap:keys:update',
-        '--no-interaction' => true,
-    ]));
-
-    $application->run(new ArrayInput([
-        'command' => 'mbin:push:keys:update',
-        '--no-interaction' => true,
-    ]));
-
-    $conn = $kernel->getContainer()->get('doctrine.orm.entity_manager')->getConnection();
-    if ($conn->isTransactionActive()) {
-        $conn->commit();
-    }
-    if ($conn->isConnected()) {
-        $conn->close();
-    }
-
-    $kernel->shutdown();
-}
-
-if (!empty($_SERVER['BOOTSTRAP_DB'])) {
-    bootstrapDatabase();
 }
