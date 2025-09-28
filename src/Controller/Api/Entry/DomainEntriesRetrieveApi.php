b9b664904bf3ae17b000deea0d060a9b5476b74c
BentiGorlich <benjamin_g@posteo.de>
2025-09-28 19:26:58 +0200

    Remove crosspost information from list API endpoints (#1731)

--- src/Controller/Api/Entry/DomainEntriesRetrieveApi.php
+++ src/Controller/Api/Entry/DomainEntriesRetrieveApi.php
@@ -157,7 +157,7 @@ class DomainEntriesRetrieveApi extends EntriesBaseApi
             try {
                 \assert($value instanceof Entry);
                 $this->handlePrivateContent($value);
-                $dtos[] = $this->serializeEntry($factory->createDto($value), $this->tagLinkRepository->getTagsOfContent($value), $this->entryRepository->findCross($value));
+                $dtos[] = $this->serializeEntry($factory->createDto($value), $this->tagLinkRepository->getTagsOfContent($value));
             } catch (\Exception $e) {
                 continue;
             }
