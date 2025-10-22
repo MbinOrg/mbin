e374d9354a037543d60c0821437bdac7800e065c
BentiGorlich <benjamin_g@posteo.de>
2025-10-22 16:50:14 +0200

    Fix tests

--- tests/Functional/Controller/Api/Magazine/Admin/MagazineDeleteIconApiTest.php
+++ tests/Functional/Controller/Api/Magazine/Admin/MagazineDeleteIconApiTest.php
@@ -93,12 +93,14 @@ class MagazineDeleteIconApiTest extends WebTestCase
 
         self::assertResponseIsSuccessful();
         $jsonData = self::getJsonResponse($this->client);
-        self::assertArrayKeysMatch(MagazineRetrieveApiTest::MAGAZINE_RESPONSE_KEYS, $jsonData);
+        self::assertArrayKeysMatch(WebTestCase::MAGAZINE_RESPONSE_KEYS, $jsonData);
         self::assertIsArray($jsonData['icon']);
         self::assertArrayKeysMatch(self::IMAGE_KEYS, $jsonData['icon']);
         self::assertSame(96, $jsonData['icon']['width']);
         self::assertSame(96, $jsonData['icon']['height']);
         self::assertEquals('a8/1c/a81cc2fea35eeb232cd28fcb109b3eb5a4e52c71bce95af6650d71876c1bcbb7.png', $jsonData['icon']['filePath']);
+
+        self::assertNull($jsonData['banner']);
 
         $this->client->request('DELETE', "/api/moderate/magazine/{$magazine->getId()}/icon", server: ['HTTP_AUTHORIZATION' => $token]);
 
