1fa9903d8a87204b325884433f2a9f1842a4fbc4
Melroy van den Berg <melroy@melroy.org>
2025-10-29 19:29:07 +0100

    Fix linting errors now

--- src/MessageHandler/Notification/SentNewSignupNotificationHandler.php
+++ src/MessageHandler/Notification/SentNewSignupNotificationHandler.php
@@ -18,8 +18,8 @@ use Symfony\Component\Messenger\Exception\UnrecoverableMessageHandlingException;
 class SentNewSignupNotificationHandler extends MbinMessageHandler
 {
     public function __construct(
-        readonly EntityManagerInterface $entityManager,
-        readonly KernelInterface $kernel,
+        public readonly EntityManagerInterface $entityManager,
+        public readonly KernelInterface $kernel,
         private readonly UserRepository $userRepository,
         private readonly SignupNotificationManager $signupNotificationManager,
     ) {
