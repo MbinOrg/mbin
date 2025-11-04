f6e9f67883145747104686965021c87e1eae04cc
Melroy van den Berg <melroy@melroy.org>
2025-11-04 11:41:33 +0100

    Update src/Service/SettingsManager.php
    Co-authored-by: BentiGorlich <benjamin_g@posteo.de>

--- src/Service/SettingsManager.php
+++ src/Service/SettingsManager.php
@@ -180,7 +180,7 @@ class SettingsManager
             $bt = debug_backtrace();
             $caller_function = ($bt[1]) ? $bt[1]['function'] : 'Unknown function caller';
 
-            $this->logger->error('SettingsManager::isBannedInstance: unable to parse host from inbox URL: {url}, called from function: {caller}', ['url' => $inboxUrl, 'caller' => $caller_function]);
+            $this->logger->error('SettingsManager::isBannedInstance: unable to parse host from URL: {url}, called from function: {caller}', ['url' => $inboxUrl, 'caller' => $caller_function]);
 
             // Do not retry, retrying will always cause a failure
             throw new UnrecoverableMessageHandlingException(\sprintf('Invalid inbox URL provided: %s', $inboxUrl));
