9e37a2ea692e0d5fb01269e3051260f0ba4d9b71
BentiGorlich <benjamin_g@posteo.de>
2025-11-18 11:21:51 +0100

    Improve ban notifications - rewrite the notification texts - add a new notification for being unbanned - use the correct method which dispatches a `NotificationCreatedEvent`

--- src/Entity/MagazineUnBanNotification.php
+++ src/Entity/MagazineUnBanNotification.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Entity]
-class MagazineBanNotification extends Notification
+class MagazineUnBanNotification extends Notification
 {
     #[ManyToOne(targetEntity: MagazineBan::class)]
     #[JoinColumn(nullable: true, onDelete: 'CASCADE')]
@@ -32,28 +32,12 @@ class MagazineBanNotification extends Notification
 
     public function getType(): string
     {
-        return 'magazine_ban_notification';
+        return 'magazine_unban_notification';
     }
 
     public function getMessage(TranslatorInterface $trans, string $locale, UrlGeneratorInterface $urlGenerator): PushNotification
     {
-        $intl = new \IntlDateFormatter($trans->getLocale(), \IntlDateFormatter::SHORT, \IntlDateFormatter::SHORT, calendar: \IntlDateFormatter::GREGORIAN);
-
-        if ($this->ban->expiredAt) {
-            $message = \sprintf('%s %s: %s. %s: %s',
-                $trans->trans('you_have_been_banned_from_magazine', ['%m' => $this->ban->magazine->name]),
-                new \DateTimeImmutable() > $this->ban->expiredAt ? $trans->trans('ban_expired') : $trans->trans('ban_expires'),
-                $intl->format($this->ban->expiredAt),
-                $trans->trans('reason'),
-                $this->ban->reason
-            );
-        } else {
-            $message = \sprintf('%s %s: %s',
-                $trans->trans('you_have_been_banned_from_magazine_permanently', ['%m' => $this->ban->magazine->name]),
-                $trans->trans('reason'),
-                $this->ban->reason
-            );
-        }
+        $message = $trans->trans('you_are_no_longer_banned_from_magazine', ['%m' => $this->ban->magazine->name]);
         $slash = $this->ban->magazine->icon && !str_starts_with('/', $this->ban->magazine->icon->filePath) ? '/' : '';
         $avatarUrl = $this->ban->magazine->icon ? '/media/cache/resolve/avatar_thumb'.$slash.$this->ban->magazine->icon->filePath : null;
 
