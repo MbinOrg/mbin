738a3725144cd52cc1106a39212f8c332c8f167a
Melroy van den Berg <melroy@melroy.org>
2025-11-27 00:26:06 +0100

    Show admin a performanc warning when enabling local only option in admin panel

--- src/Controller/Admin/AdminSettingsController.php
+++ src/Controller/Admin/AdminSettingsController.php
@@ -7,13 +7,17 @@ namespace App\Controller\Admin;
 use App\Controller\AbstractController;
 use App\Form\SettingsType;
 use App\Service\SettingsManager;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AdminSettingsController extends AbstractController
 {
-    public function __construct(private readonly SettingsManager $settings)
+    public function __construct(
+        private readonly SettingsManager $settings,
+        private readonly TranslatorInterface $translator)
     {
     }
 
@@ -23,6 +27,11 @@ class AdminSettingsController extends AbstractController
         $dto = $this->settings->getDto();
 
         $form = $this->createForm(SettingsType::class, $dto);
+
+        if ($dto->MBIN_SIDEBAR_SECTIONS_LOCAL_ONLY) {
+            $form->get('MBIN_SIDEBAR_SECTIONS_LOCAL_ONLY')->addError(new FormError($this->translator->trans('local_only_performance_warning')));
+        }
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
