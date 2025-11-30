d09c5998cfb87e50ef1a78c7c70d85e6fb49f01a
blued_gear <chocolatecakecodes@disroot.org>
2025-11-30 17:58:59 +0000

    make posts in combined view similar to threads

---
+++ src/Twig/Components/PostCombinedComponent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('post_combined')]
+class PostCombinedComponent extends PostComponent
+{
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        parent::__construct($authorizationChecker);
+    }
+
+}
