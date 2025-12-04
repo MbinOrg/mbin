c2437c5dbf67d0b9e1747d91ae9291e6ce7da98c
blued_gear <chocolatecakecodes@disroot.org>
2025-12-04 09:15:13 +0000

    please the linter

--- src/Exception/RetryMessageException.php
+++ src/Exception/RetryMessageException.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Exception;
 
 use JetBrains\PhpStorm\Pure;
 
 /**
- * Thrown when something is not as expected but might succeed when retried later
+ * Thrown when something is not as expected but might succeed when retried later.
  */
 final class RetryMessageException extends \Exception
 {
