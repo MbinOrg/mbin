79fbe5933b3df7c63fbc67703e6aacbfc3f9a9e5
BentiGorlich <benjamin_g@posteo.de>
2026-01-14 23:28:10 +0100

    Introducing monitoring - add monitoring capabilities for troubleshooting purposes. This cannot be enabled or disabled from the UI, you have to go into the `.env` files for this - Add monitoring capabilities for the biggest factors: curl requests (AP), twig rendering (frontend) and of course database querying - 2 sources can start an execution context: an incoming request and a started message - added an admin UI for inspecting requests and their response times, with an overview grouped by route name

---
+++ src/Entity/MonitoringQuery.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Entity\Traits\CreatedAtTrait;
+use App\Entity\Traits\MonitoringPerformanceTrait;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+
+#[Entity]
+class MonitoringQuery
+{
+    use CreatedAtTrait {
+        CreatedAtTrait::__construct as createdAtTraitConstruct;
+    }
+    use MonitoringPerformanceTrait;
+
+    #[Id]
+    #[GeneratedValue]
+    #[Column(type: 'integer')]
+    private int $id;
+
+    #[Column(type: 'text')]
+    public string $query;
+
+    #[Column(type: 'json', nullable: true, options: ['jsonb' => true])]
+    public ?array $parameters = null;
+
+    #[ManyToOne(targetEntity: MonitoringExecutionContext::class, inversedBy: 'queries')]
+    #[JoinColumn(referencedColumnName: 'uuid', onDelete: 'CASCADE')]
+    public MonitoringExecutionContext $context;
+
+    public function __construct()
+    {
+        $this->createdAtTraitConstruct();
+    }
+}
