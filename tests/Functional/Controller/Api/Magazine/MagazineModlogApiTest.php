2bb2007dc14a9c81c83714a3e7ac6591b9148e46
BentiGorlich <benjamin_g@posteo.de>
2026-01-14 12:51:50 +0100

    Fix magazine log api subject (#1951)

--- tests/Functional/Controller/Api/Magazine/MagazineModlogApiTest.php
+++ tests/Functional/Controller/Api/Magazine/MagazineModlogApiTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Controller\Api\Magazine;
 
+use App\DTO\MagazineBanDto;
+use App\DTO\ModeratorDto;
 use App\Entity\MagazineLog;
 use App\Tests\WebTestCase;
 
@@ -58,6 +60,99 @@ class MagazineModlogApiTest extends WebTestCase
         self::assertArrayKeysMatch(self::PAGINATED_KEYS, $jsonData);
         self::assertIsArray($jsonData['items']);
         self::assertEmpty($jsonData['items']);
+    }
+
+    public function testApiCanRetrieveEntryPinnedLog(): void
+    {
+        $user = $this->getUserByUsername('JohnDoe');
+        $this->client->loginUser($user);
+        self::createOAuth2AuthCodeClient();
+
+        $magazine = $this->getMagazineByName('test');
+        $entry = $this->getEntryByTitle('Something to pin', magazine: $magazine);
+        $this->entryManager->pin($entry, $user);
+
+        $codes = self::getAuthorizationCodeTokenResponse($this->client);
+        $token = $codes['token_type'].' '.$codes['access_token'];
+
+        $this->client->request('GET', '/api/magazine/'.$magazine->getId().'/log', server: ['HTTP_AUTHORIZATION' => $token]);
+
+        self::assertResponseIsSuccessful();
+        $jsonData = self::getJsonResponse($this->client);
+
+        self::assertIsArray($jsonData);
+        self::assertArrayKeysMatch(self::PAGINATED_KEYS, $jsonData);
+        self::assertIsArray($jsonData['items']);
+        self::assertCount(1, $jsonData['items']);
+        $item = $jsonData['items'][0];
+        self::assertArrayKeysMatch(WebTestCase::LOG_ENTRY_KEYS, $item);
+        self::assertEquals('log_entry_pinned', $item['type']);
+        self::assertIsArray($item['subject']);
+        self::assertArrayKeysMatch(WebTestCase::ENTRY_RESPONSE_KEYS, $item['subject']);
+        self::assertEquals($entry->getId(), $item['subject']['entryId']);
+    }
+
+    public function testApiCanRetrieveUserBannedLog(): void
+    {
+        $user = $this->getUserByUsername('JohnDoe');
+        $this->client->loginUser($user);
+        self::createOAuth2AuthCodeClient();
+
+        $magazine = $this->getMagazineByName('test');
+        $banned = $this->getUserByUsername('troll');
+        $dto = new MagazineBanDto();
+        $dto->reason = 'because';
+        $ban = $this->magazineManager->ban($magazine, $banned, $user, $dto);
+
+        $codes = self::getAuthorizationCodeTokenResponse($this->client);
+        $token = $codes['token_type'].' '.$codes['access_token'];
+
+        $this->client->request('GET', '/api/magazine/'.$magazine->getId().'/log', server: ['HTTP_AUTHORIZATION' => $token]);
+
+        self::assertResponseIsSuccessful();
+        $jsonData = self::getJsonResponse($this->client);
+
+        self::assertIsArray($jsonData);
+        self::assertArrayKeysMatch(self::PAGINATED_KEYS, $jsonData);
+        self::assertIsArray($jsonData['items']);
+        self::assertCount(1, $jsonData['items']);
+        $item = $jsonData['items'][0];
+        self::assertArrayKeysMatch(WebTestCase::LOG_ENTRY_KEYS, $item);
+        self::assertEquals('log_ban', $item['type']);
+        self::assertArrayKeysMatch(WebTestCase::BAN_RESPONSE_KEYS, $item['subject']);
+        self::assertEquals($ban->getId(), $item['subject']['banId']);
+    }
+
+    public function testApiCanRetrieveModeratorAddedLog(): void
+    {
+        $user = $this->getUserByUsername('JohnDoe');
+        $this->client->loginUser($user);
+        self::createOAuth2AuthCodeClient();
+
+        $magazine = $this->getMagazineByName('test');
+        $mod = $this->getUserByUsername('mod');
+        $dto = new ModeratorDto($magazine, $mod, $user);
+        $this->magazineManager->addModerator($dto);
+
+        $codes = self::getAuthorizationCodeTokenResponse($this->client);
+        $token = $codes['token_type'].' '.$codes['access_token'];
+
+        $this->client->request('GET', '/api/magazine/'.$magazine->getId().'/log', server: ['HTTP_AUTHORIZATION' => $token]);
+
+        self::assertResponseIsSuccessful();
+        $jsonData = self::getJsonResponse($this->client);
+
+        self::assertIsArray($jsonData);
+        self::assertArrayKeysMatch(self::PAGINATED_KEYS, $jsonData);
+        self::assertIsArray($jsonData['items']);
+        self::assertCount(1, $jsonData['items']);
+        $item = $jsonData['items'][0];
+        self::assertArrayKeysMatch(WebTestCase::LOG_ENTRY_KEYS, $item);
+        self::assertEquals('log_moderator_add', $item['type']);
+        self::assertArrayKeysMatch(WebTestCase::USER_SMALL_RESPONSE_KEYS, $item['subject']);
+        self::assertEquals($mod->getId(), $item['subject']['userId']);
+        self::assertArrayKeysMatch(WebTestCase::USER_SMALL_RESPONSE_KEYS, $item['moderator']);
+        self::assertEquals($user->getId(), $item['moderator']['userId']);
     }
 
     public function testApiModlogReflectsModerationActionsTaken(): void
