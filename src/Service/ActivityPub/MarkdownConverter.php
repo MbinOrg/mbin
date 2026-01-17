54db9080a1db4d340890a6cc2f16eb9c605a8f95
BentiGorlich <benjamin_g@posteo.de>
2026-01-17 21:31:47 +0100

    Fixing Mastodon mentions again again again (#1954)

--- src/Service/ActivityPub/MarkdownConverter.php
+++ src/Service/ActivityPub/MarkdownConverter.php
@@ -33,7 +33,7 @@ class MarkdownConverter
 
         foreach ($matches as $match) {
             if ($this->mentionManager->extract($match[1])) {
-                $mentionFromTag = array_filter($apTags, fn ($tag) => 'Mention' === ($tag['type'] ?? '') && $match[2] === ($tag['href'] ?? ''));
+                $mentionFromTag = $this->findMentionFromTag($match, $apTags);
                 if (\count($mentionFromTag)) {
                     $mentionFromTagObj = $mentionFromTag[array_key_first($mentionFromTag)];
                     $mentioned = null;
@@ -51,7 +51,8 @@ class MarkdownConverter
                 } else {
                     try {
                         $actor = $this->activityPubManager->findActorOrCreate($match[2]);
-                        $replace = '@'.($actor instanceof User ? $actor->username : $actor->name);
+                        $username = $actor instanceof User ? $actor->username : $actor->name;
+                        $replace = $this->mentionManager->getUsername($username, true);
                     } catch (\Throwable) {
                         $replace = $match[1];
                     }
@@ -65,5 +66,26 @@ class MarkdownConverter
         }
 
         return $value;
+    }
+
+    private function findMentionFromTag(array $match, array $apTags): array
+    {
+        $res = [];
+        foreach ($apTags as $tag) {
+            if ('Mention' === $tag['type']) {
+                if ($match[2] === $tag['href']) {
+                    // the href in the tag array might be the same as the link from the text
+                    $res[] = $tag;
+                } elseif ($match[1] === $tag['name']) {
+                    // or it might not be, but the linktext from the text might be the same as the name in the tag array
+                    $res[] = $tag;
+                } elseif (($host = parse_url($tag['href'], PHP_URL_HOST)) && "$match[1]@$host" === $tag['name']) {
+                    // or the tag array might contain the full handle, but the linktext might only be the name part of the handle
+                    $res[] = $tag;
+                }
+            }
+        }
+
+        return $res;
     }
 }
