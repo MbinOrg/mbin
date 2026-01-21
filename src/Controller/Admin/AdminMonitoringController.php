a7e434ff7386fcc729f80d0d276ed2047e522960
BentiGorlich <benjamin_g@posteo.de>
2026-01-21 13:00:29 +0100

    Add filtering, fix some things - Add filtering form and dto to the monitoring overview - Fix `Could not convert PHP type 'array' to 'json', as an 'Malformed UTF-8 characters, possibly incorrectly encoded' error was triggered by the serialization` and add a test for it - move chart data generation to the controller

--- src/Controller/Admin/AdminMonitoringController.php
+++ src/Controller/Admin/AdminMonitoringController.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Controller\AbstractController;
+use App\DTO\MonitoringExecutionContextFilterDto;
+use App\Form\MonitoringExecutionContextFilterType;
 use App\Repository\MonitoringRepository;
-use Doctrine\Common\Collections\Criteria;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
 
@@ -18,19 +22,47 @@ class AdminMonitoringController extends AbstractController
     public function __construct(
         private readonly MonitoringRepository $monitoringRepository,
         private readonly ChartBuilderInterface $chartBuilder,
+        private readonly FormFactoryInterface $formFactory,
+        private readonly TranslatorInterface $translator,
     ) {
     }
 
     #[IsGranted('ROLE_ADMIN')]
-    public function overview(#[MapQueryParameter] int $p = 1): Response
+    public function overview(Request $request, #[MapQueryParameter] int $p = 1): Response
     {
-        $criteria = new Criteria(orderings: ['createdAt' => 'DESC']);
-        $contexts = $this->monitoringRepository->findByPaginated($criteria);
+        $dto = new MonitoringExecutionContextFilterDto();
+        $form = $this->formFactory->createNamed('filter', MonitoringExecutionContextFilterType::class, $dto, ['method' => 'GET']);
+        $hideChart = false;
+
+        try {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $dto = $form->getData();
+                $hideChart = true;
+            }
+        } catch (\Exception) {
+        }
+
+        $contexts = $this->monitoringRepository->getFilteredContextsPaginated($dto);
         $contexts->setCurrentPage($p);
         $contexts->setMaxPerPage(50);
 
+        if (1 === $p && !$hideChart) {
+            $chart = $this->getOverViewChart();
+        }
+
+        return $this->render('admin/monitoring/monitoring.html.twig', [
+            'page' => $p,
+            'executionContexts' => $contexts,
+            'chart' => $chart ?? null,
+            'form' => $form,
+        ]);
+    }
+
+    private function getOverViewChart(): Chart
+    {
         $chart = $this->chartBuilder->createChart(Chart::TYPE_BAR);
-        $chart->setData($this->monitoringRepository->getChartData());
+        $chart->setData($this->getOverviewChartData());
         $chart->setOptions([
             'scales' => [
                 'y' => [
@@ -48,10 +80,63 @@ class AdminMonitoringController extends AbstractController
             ],
         ]);
 
-        return $this->render('admin/monitoring/monitoring.html.twig', [
-            'executionContexts' => $contexts,
-            'chart' => $chart,
-        ]);
+        return $chart;
+    }
+
+    public function getOverviewChartData(): array
+    {
+        $rawData = $this->monitoringRepository->getOverviewRouteCalls();
+        $labels = [];
+        $overallDurationRemaining = [];
+        $queryDurations = [];
+        $twigRenderDurationRemaining = [];
+        $curlRequestDurationRemaining = [];
+
+        foreach ($rawData as $data) {
+            $labels[] = $data['path'];
+            $total = round(\floatval($data['total_duration']), 2);
+            $query = round(\floatval($data['query_duration']), 2);
+            $twig = round(\floatval($data['twig_render_duration']), 2);
+            $curl = round(\floatval($data['curl_request_duration']), 2);
+            $overallDurationRemaining[] = max(0, round($total - $query - $twig - $curl, 2));
+            $queryDurations[] = $query;
+            $twigRenderDurationRemaining[] = $twig;
+            $curlRequestDurationRemaining[] = $curl;
+        }
+
+        return [
+            'labels' => $labels,
+            'datasets' => [
+                [
+                    'label' => $this->translator->trans('monitoring_duration_overall'),
+                    'data' => $overallDurationRemaining,
+                    'stack' => '1',
+                    'backgroundColor' => 'gray',
+                    'borderRadius' => 5,
+                ],
+                [
+                    'label' => $this->translator->trans('monitoring_duration_query'),
+                    'data' => $queryDurations,
+                    'stack' => '1',
+                    'backgroundColor' => '#a3067c',
+                    'borderRadius' => 5,
+                ],
+                [
+                    'label' => $this->translator->trans('monitoring_duration_twig_render'),
+                    'data' => $twigRenderDurationRemaining,
+                    'stack' => '1',
+                    'backgroundColor' => 'green',
+                    'borderRadius' => 5,
+                ],
+                [
+                    'label' => $this->translator->trans('monitoring_duration_curl_request'),
+                    'data' => $curlRequestDurationRemaining,
+                    'stack' => '1',
+                    'backgroundColor' => '#07abaf',
+                    'borderRadius' => 5,
+                ],
+            ],
+        ];
     }
 
     #[IsGranted('ROLE_ADMIN')]
