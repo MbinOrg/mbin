91b0a94818e0c65221ca7eb8be12a44f0e24b98e
BentiGorlich <benjamin_g@posteo.de>
2026-01-23 16:35:14 +0100

    update recipe symfony/webpack-encore-bundle

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,10 @@ Encore
      * https://symfony.com/doc/current/frontend.html#adding-more-features
      */
     .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
+
+    // Displays build status system notifications to the user
+    // .enableBuildNotifications()
+
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
