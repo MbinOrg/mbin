09752db35d637c68b50509698026870ea9959967
BentiGorlich <benjamin_g@posteo.de>
2026-01-31 20:59:14 +0100

    Move the front page to cursor based pagination - instead of the usual `LIMIT`/`OFFSET` pagination use cursor based one based on the current ordering - Write classes that are inspired by the pagination of doctrine for cursor based pagination - make the `ContentRepository` work with the new pagination, while still working with the regular one

--- src/Controller/Entry/EntryFrontController.php
+++ src/Controller/Entry/EntryFrontController.php
@@ -43,6 +43,7 @@ class EntryFrontController extends AbstractController
         #[MapQueryParameter]
         ?string $type,
         Request $request,
+        #[MapQueryParameter] ?string $cursor = null,
     ): Response {
         $user = $this->getUser();
 
@@ -63,7 +64,8 @@ class EntryFrontController extends AbstractController
             $criteria->fetchCachedItems($this->sqlHelpers, $user);
         }
 
-        $entities = $this->contentRepository->findByCriteria($criteria);
+        $entities = $this->contentRepository->findByCriteriaCursored($criteria, $this->getCursorByCriteria($criteria, $cursor));
+        $page = $entities->getCurrentPageResults();
         $templatePath = 'content/';
         $dataKey = 'results';
 
@@ -108,6 +110,7 @@ class EntryFrontController extends AbstractController
         #[MapQueryParameter]
         ?string $type,
         Request $request,
+        #[MapQueryParameter] ?string $cursor = null,
     ): Response {
         $user = $this->getUser();
         $response = new Response();
@@ -134,7 +137,7 @@ class EntryFrontController extends AbstractController
         return $this->renderResponse(
             $request,
             $criteria,
-            ['results' => $this->contentRepository->findByCriteria($criteria), 'magazine' => $magazine],
+            ['results' => $this->contentRepository->findByCriteriaCursored($criteria, $this->getCursorByCriteria($criteria, $cursor)), 'magazine' => $magazine],
             'content/',
             $user
         );
@@ -300,5 +303,23 @@ class EntryFrontController extends AbstractController
         $pagerfanta->setCurrentPageResults($results);
 
         return $pagerfanta;
+    }
+
+    /**
+     * @throws \DateMalformedStringException
+     */
+    private function getCursorByCriteria(Criteria $criteria, ?string $cursor): int|\DateTimeImmutable
+    {
+        $guessedCursor = $this->contentRepository->guessInitialCursor($criteria);
+        if ($guessedCursor instanceof \DateTimeImmutable) {
+            $currentCursor = null !== $cursor ? new \DateTimeImmutable($cursor) : $guessedCursor;
+        // $currentCursor = null !== $cursor ? (new \DateTimeImmutable)->setTimestamp(intval($cursor)) : $guessedCursor;
+        } elseif (\is_int($guessedCursor)) {
+            $currentCursor = null !== $cursor ? \intval($cursor) : $guessedCursor;
+        } else {
+            throw new \LogicException(\get_class($guessedCursor).' is not accounted for');
+        }
+
+        return $currentCursor;
     }
 }
