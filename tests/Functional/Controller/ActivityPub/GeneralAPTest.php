fd7a24ecc9600448b22488990e73df5b28558eda
BentiGorlich <benjamin_g@posteo.de>
2026-02-24 17:01:53 +0100

    Expand test cases

--- tests/Functional/Controller/ActivityPub/GeneralAPTest.php
+++ tests/Functional/Controller/ActivityPub/GeneralAPTest.php
@@ -5,29 +5,31 @@ declare(strict_types=1);
 namespace App\Tests\Functional\Controller\ActivityPub;
 
 use App\Tests\WebTestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class GeneralAPTest extends WebTestCase
 {
-    public function testResponseToApProfile(): void
+    #[DataProvider('provideAcceptHeaders')]
+    public function testResponseToApProfile(string $acceptHeader): void
     {
         $user = $this->getUserByUsername('user');
 
         $this->client->request('GET', '/u/user', [], [], [
-            'HTTP_ACCEPT' => 'application/ld+json;profile=https://www.w3.org/ns/activitystreams',
+            'HTTP_ACCEPT' => $acceptHeader,
         ]);
 
         self::assertResponseHeaderSame('Content-Type', 'application/activity+json');
+    }
 
-        $this->client->request('GET', '/u/user', [], [], [
-            'HTTP_ACCEPT' => 'application/ld+json;profile="https://www.w3.org/ns/activitystreams"',
-        ]);
-
-        self::assertResponseHeaderSame('Content-Type', 'application/activity+json');
-
-        $this->client->request('GET', '/u/user', [], [], [
-            'HTTP_ACCEPT' => 'application/ld+json ; profile="https://www.w3.org/ns/activitystreams"',
-        ]);
-
-        self::assertResponseHeaderSame('Content-Type', 'application/activity+json');
+    public static function provideAcceptHeaders(): array
+    {
+        return [
+            ['application/ld+json;profile=https://www.w3.org/ns/activitystreams'],
+            ['application/ld+json;profile="https://www.w3.org/ns/activitystreams"'],
+            ['application/ld+json ; profile="https://www.w3.org/ns/activitystreams"'],
+            ['application/ld+json'],
+            ['application/activity+json'],
+            ['application/json'],
+        ];
     }
 }
