3a26c660ddecb5ea4c4e6ff6792947d6abb509de
blued_gear <164888202+blued-gear@users.noreply.github.com>
2026-04-13 16:35:18 +0000

    fix query for boosted comments + better layout for post comments in combined (#2090)
    Co-authored-by: BentiGorlich <benjamin_g@posteo.de>

---
+++ src/Twig/Components/PostCommentCombinedComponent.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Components;
+
+use App\Entity\Contracts\VisibilityInterface;
+use App\Entity\PostComment;
+use App\Repository\Criteria;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('post_comment_combined')]
+final class PostCommentCombinedComponent
+{
+    public function __construct(
+        private readonly AuthorizationCheckerInterface $authorizationChecker,
+    ) {
+    }
+
+    public PostComment $comment;
+    public bool $dateAsUrl = true;
+    public bool $showNested = false;
+    public bool $withPost = false;
+    public int $level = 1;
+    public bool $canSeeTrash = false;
+    public Criteria $criteria;
+
+    public function postMount(array $attr): array
+    {
+        $this->canSeeTrashed();
+
+        return $attr;
+    }
+
+    public function canSeeTrashed(): bool
+    {
+        if (VisibilityInterface::VISIBILITY_VISIBLE === $this->comment->visibility) {
+            return true;
+        }
+
+        if (VisibilityInterface::VISIBILITY_TRASHED === $this->comment->visibility
+            && $this->authorizationChecker->isGranted(
+                'moderate',
+                $this->comment
+            )
+            && $this->canSeeTrash) {
+            return true;
+        }
+
+        $this->comment->image = null;
+
+        return false;
+    }
+}
