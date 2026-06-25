e1eed09fb373264614550f070de5779f0a4b660a
blued_gear <chocolatecakecodes@disroot.org>
2026-06-25 00:18:08 +0000

    update Symfony part 8

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,6 +1,7 @@
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
-import { startStimulusApp } from '@symfony/stimulus-bridge';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
