36159c17545dd75eeca904e1e4e293e404da24db
blued_gear <chocolatecakecodes@disroot.org>
2026-06-27 21:13:33 +0000

    fix deprecations

--- src/Twig/Components/ReportListComponent.php
+++ src/Twig/Components/ReportListComponent.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Twig\Components;
 
+use App\Utils\Polyfills;
 use Pagerfanta\PagerfantaInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent('report_list', template: 'components/report_list.html.twig')]
@@ -13,4 +15,13 @@ final class ReportListComponent
     public PagerfantaInterface $reports;
     public string $routeName = 'admin_reports';
     public ?string $magazineName = null;
+
+    public function __construct(
+        private readonly RequestStack $requestStack,
+    ) {}
+
+    public function getStatus(): string {
+        return Polyfills::requestParam($this->requestStack->getCurrentRequest(), 'status');
+    }
+
 }
