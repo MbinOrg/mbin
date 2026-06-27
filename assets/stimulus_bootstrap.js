e5d838e1b47bbf293a9e3f9982904e3440782d22
blued_gear <chocolatecakecodes@disroot.org>
2026-06-27 17:39:30 +0000

    update recipe

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,11 +1,7 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
-
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
-    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
-    true,
-    /\.[jt]sx?$/,
-));
+export const app = startStimulusApp(import.meta.webpackContext('@symfony/stimulus-bridge/lazy-controller-loader!./controllers', {
+    recursive: true,
+    regExp: /\.[jt]sx?$/,
+}));
