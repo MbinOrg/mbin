4682468fe9311bef32af1fc64c5356547cb0c86e
blued_gear <chocolatecakecodes@disroot.org>
2026-07-23 13:11:08 +0000

    fix frankenphp config

--- public/index.php
+++ public/index.php
@@ -6,6 +6,6 @@ use App\Kernel;
 
 require_once \dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return static function (array $context) {
+return static function (array $context): Kernel {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
