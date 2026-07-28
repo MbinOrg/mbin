4afca4a79af2d088b913f8279fc135b53ee33647
blued_gear <164888202+blued-gear@users.noreply.github.com>
2026-07-28 06:20:30 +0000

    Add ability for users to block whole instances (#2155)
    Co-authored-by: Garrett W. <gw@garrettw.net>

---
+++ src/Event/InstancesGlobalBlockedEvent.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Event;
+
+use App\Entity\Instance;
+
+/**
+ * @psalm-immutable
+ */
+class InstancesGlobalBlockedEvent
+{
+    /**
+     * @param Instance[] $instances
+     *
+     * @psalm-mutation-free
+     */
+    public function __construct(public array $instances)
+    {
+    }
+}
