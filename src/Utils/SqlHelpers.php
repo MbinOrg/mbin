4106b693edff20bae8e5b7973c5e933f13bdfa3d
blued_gear <164888202+blued-gear@users.noreply.github.com>
2026-08-03 18:55:40 +0000

    a couple of smaller fixes (#2187)
    (cherry picked from commit c2e1b2185766a75fc2054ff94712c7979520693d)

--- src/Utils/SqlHelpers.php
+++ src/Utils/SqlHelpers.php
@@ -105,6 +105,8 @@ class SqlHelpers
             return Types::DATETIMETZ_MUTABLE;
         } elseif (\is_int($value)) {
             return Types::INTEGER;
+        } elseif (\is_bool($value)) {
+            return Types::BOOLEAN;
         }
 
         return Types::STRING;
