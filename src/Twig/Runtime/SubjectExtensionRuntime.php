2f9e7692d79695aef3ab7f082deae7e0d19473d2
blued_gear <chocolatecakecodes@disroot.org>
2026-09-09 20:30:09 +0000

    hide entry and post title if they should not be shown to the user

--- src/Twig/Runtime/SubjectExtensionRuntime.php
+++ src/Twig/Runtime/SubjectExtensionRuntime.php
@@ -4,17 +4,72 @@ declare(strict_types=1);
 
 namespace App\Twig\Runtime;
 
+use App\Entity\Entry;
+use App\Entity\Post;
+use App\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
 use Twig\Extension\RuntimeExtensionInterface;
 
-class SubjectExtensionRuntime implements RuntimeExtensionInterface
+readonly class SubjectExtensionRuntime implements RuntimeExtensionInterface
 {
-    public function __construct()
-    {
-        // Inject dependencies if needed
+    public function __construct(
+        private Security $security,
+    ) {
     }
 
-    public function doSomething($value)
+    public function userCanSeeEntry(Entry $entry): bool
     {
-        // ...
+        /* @var ?User $user */
+        $user = $this->security->getUser();
+
+        if (null !== $user) {
+            if ($user->isBlocked($entry->user)
+                || $user->isBlockedMagazine($entry->getMagazine())
+                || (null !== $entry->domain && $user->isBlockedDomain($entry->domain))) {
+                return false;
+            }
+        }
+
+        if ($entry->isVisible() && $entry->user->isVisible()) {
+            return true;
+        }
+
+        if (null !== $user) {
+            if ($user->isAdmin() || $user->isModerator()) {
+                return true;
+            }
+            if ($entry->getMagazine()->userIsModerator($user)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function userCanSeePost(Post $post): bool
+    {
+        /* @var ?User $user */
+        $user = $this->security->getUser();
+
+        if (null !== $user) {
+            if ($user->isBlocked($post->user) || $user->isBlockedMagazine($post->getMagazine())) {
+                return false;
+            }
+        }
+
+        if ($post->isVisible() && $post->user->isVisible()) {
+            return true;
+        }
+
+        if (null !== $user) {
+            if ($user->isAdmin() || $user->isModerator()) {
+                return true;
+            }
+            if ($post->getMagazine()->userIsModerator($user)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
