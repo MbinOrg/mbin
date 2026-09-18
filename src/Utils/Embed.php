409fd77fb2a11d0efa9e87bdd6632773073fa73e
Melroy van den Berg <melroy@melroy.org>
2026-09-18 21:17:53 +0200

    Block private network access in embed fetches

--- src/Utils/Embed.php
+++ src/Utils/Embed.php
@@ -12,10 +12,14 @@ use App\Service\SettingsManager;
 use App\Service\VideoManager;
 use Embed\Embed as BaseEmbed;
 use Embed\Extractor;
+use Embed\Http\Crawler;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpClient\NoPrivateNetworkHttpClient;
+use Symfony\Component\HttpClient\Psr18Client;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class Embed
 {
@@ -30,6 +34,7 @@ class Embed
         private SettingsManager $settings,
         private LoggerInterface $logger,
         private EventDispatcherInterface $dispatcher,
+        private HttpClientInterface $httpClient,
     ) {
     }
 
@@ -39,6 +44,7 @@ class Embed
         unset($this->settings);
         unset($this->logger);
         unset($this->dispatcher);
+        unset($this->httpClient);
     }
 
     public function fetch(string $url): self
@@ -108,7 +114,15 @@ class Embed
 
     private function fetchEmbed(string $url): Extractor
     {
-        $fetcher = new BaseEmbed();
+        $httpClient = new NoPrivateNetworkHttpClient(
+            $this->httpClient->withOptions([
+                'max_redirects' => 10,
+                'max_duration' => 10,
+                'timeout' => 10,
+            ])
+        );
+        $psr18Client = new Psr18Client($httpClient);
+        $fetcher = new BaseEmbed(new Crawler($psr18Client, $psr18Client, $psr18Client));
         $embed = $fetcher->get($url);
 
         if ($this->detectFaultyRedirectEmbed($embed)) {
