707dbca97c8f99df5a1f1dcdca6446afa3aa3cf6
blued_gear <chocolatecakecodes@disroot.org>
2026-09-22 22:28:56 +0000

    apply review suggestion

--- src/Message/DeleteImageMessage.php
+++ src/Message/DeleteImageMessage.php
@@ -7,8 +7,9 @@ namespace App\Message;
 use App\Message\Contracts\AsyncMessageInterface;
 
 /**
- * Attempts to delete the image entity from the database, but not the file from storage.
+ * Attempts to delete the image entity from the database and the file from storage.
  */
+#[\Deprecated]
 class DeleteImageMessage implements AsyncMessageInterface
 {
     public function __construct(public int $id, public bool $force = false)
