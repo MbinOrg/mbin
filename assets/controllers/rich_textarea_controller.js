7c96db5880d69f37736fd1a95d7666bab4c398bc
BentiGorlich <benjamin_g@posteo.de>
2026-09-22 18:44:59 +0200

    Fix null error on initialization - The abortController is undefined in the beginning, so make the call null-safe

--- assets/controllers/rich_textarea_controller.js
+++ assets/controllers/rich_textarea_controller.js
@@ -119,7 +119,7 @@ export default class extends Controller {
         this.selectedSuggestionIndex = 0;
         this.emojiAutocompleteActive = false;
         this.mentionAutocompleteActive = false;
-        this.abortController.abort();
+        this.abortController?.abort();
         this.requestActive = false;
         document.getElementById('user-suggestions')?.remove();
         document.getElementById('emoji-suggestions')?.remove();
@@ -165,7 +165,7 @@ export default class extends Controller {
 
     fetchAutocompleteResults(searchText) {
         if (this.requestActive) {
-            this.abortController.abort();
+            this.abortController?.abort();
         }
 
         if (this.mentionAutocompleteActive) {
