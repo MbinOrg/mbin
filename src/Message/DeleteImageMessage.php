f54b3e8524b09c3bb7001480cf6697824fe0faf1
blued_gear <chocolatecakecodes@disroot.org>
2026-09-23 20:04:52 +0000

    fix build

--- src/Message/DeleteImageMessage.php
+++ src/Message/DeleteImageMessage.php
@@ -8,8 +8,9 @@ use App\Message\Contracts\AsyncMessageInterface;
 
 /**
  * Attempts to delete the image entity from the database and the file from storage.
+ *
+ * @deprecated use DeleteImageV2Message::fromImage() instead
  */
-#[\Deprecated]
 class DeleteImageMessage implements AsyncMessageInterface
 {
     public function __construct(public int $id, public bool $force = false)
